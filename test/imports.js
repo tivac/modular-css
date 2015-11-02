075d715f6a3703268fb73eee973f1a90e86887ee
Pat Cavit <pcavit@gmail.com>
2015-11-02 13:04:03 -0800

    Test import response w/ a hierarchy

--- test/imports.js
+++ test/imports.js
@@ -23,6 +23,15 @@ describe("postcss-css-modules", function() {
             assert(path.join(__dirname, "./specimens/imports/node_modules.css") in result.files);
             assert(path.join(__dirname, "./specimens/imports/node_modules/test-styles/styles.css") in result.files);
         });
+
+        it("should export identifiers and their classes", function() {
+            var result  = imports.process("./test/specimens/imports/start.css"),
+                exports = result.exports;
+
+            assert.deepEqual(exports, {
+                wooga : [ "771db310e8d1c8c48a9d4d3e5c0af682_booga" ],
+                booga : [ "82a0a82265094a412fd49bf12216eac5_booga" ]
+            });
         });
     });
 });
