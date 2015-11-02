9f0a6454edea9174386bd76ab41650ee6f9b525d
Pat Cavit <pcavit@gmail.com>
2015-11-02 15:24:54 -0800

    Add an actual (very basic) CLI

---
+++ bin/cli.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var fs   = require("fs"),
+    path = require("path"),
+
+    imports = require("../imports"),
+
+    result, out;
+
+result = imports.process(process.argv[2]);
+
+if(!process.argv[3]) {
+    return console.log(result.css);
+}
+
+out = process.argv[3];
+
+fs.writeFileSync(out, result.css, "utf8");
+fs.writeFileSync(
+    path.join(path.dirname(out), path.basename(out, path.extname(out)) + ".json"),
+    JSON.stringify(result.exports),
+    "utf8"
+);
