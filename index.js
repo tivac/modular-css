1ade1baf8a669bca9394bdf58be1834ec1061c7d
Pat Cavit <pcavit@gmail.com>
2015-11-02 15:25:29 -0800

    Export the import API as `process()`

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 "use strict";
 
-var fs = require("fs"),
-
-    postcss = require("postcss");
-
-postcss([
-    require("./plugins/values.js"),
-    require("./plugins/scoping.js"),
-    require("./plugins/composition.js")
-])
-.process(fs.readFileSync(process.argv[2], { encoding: "utf8" }))
-.then(function(result) {
-    console.log(result.css);
-}, console.log.bind(console));
+exports.process = require("./imports").process;
