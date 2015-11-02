0e1d8b0b737d1833eb28098e7c5b330cb3b60e3a
Pat Cavit <pcavit@gmail.com>
2015-11-02 14:54:46 -0800

    More tests for full importing path
    To help test error states & nested value hierarchies

--- test/imports.js
+++ test/imports.js
@@ -7,14 +7,14 @@ var path   = require("path"),
 
 describe("postcss-css-modules", function() {
     describe("imports", function() {
+        var start = imports.process("./test/specimens/imports/start.css");
+
         it("should walk dependencies", function() {
-            var result = imports.process("./test/specimens/imports/start.css");
+            assert("files" in start);
             
-            assert("files" in result);
-            
-            assert(path.join(__dirname, "./specimens/imports/start.css") in result.files);
-            assert(path.join(__dirname, "./specimens/imports/local.css") in result.files);
-            assert(path.join(__dirname, "./specimens/imports/folder/folder.css") in result.files);
+            assert(path.join(__dirname, "./specimens/imports/start.css") in start.files);
+            assert(path.join(__dirname, "./specimens/imports/local.css") in start.files);
+            assert(path.join(__dirname, "./specimens/imports/folder/folder.css") in start.files);
         });
         
         it("should walk dependencies into node_modules", function() {
@@ -25,12 +25,36 @@ describe("postcss-css-modules", function() {
         });
 
         it("should export identifiers and their classes", function() {
-            var result  = imports.process("./test/specimens/imports/start.css"),
-                exports = result.exports;
+            assert.deepEqual(start.exports, {
+                wooga : [ "e90a9ca9bb862787d5423d08ffa8a320_booga" ],
+                booga : [ "624cf273647af7597f5866483cde95d0_booga" ],
+                tooga : [ "624cf273647af7597f5866483cde95d0_tooga" ]
+            });
+        });
 
-            assert.deepEqual(exports, {
-                wooga : [ "771db310e8d1c8c48a9d4d3e5c0af682_booga" ],
-                booga : [ "82a0a82265094a412fd49bf12216eac5_booga" ]
+        it("should generate correct css", function() {
+            // normalize newlines
+            var css = start.css.replace(/\r\n/g, "\n");
+
+            assert.equal(css.indexOf(".b5600c78a155fc10eeddec15127659a8_folder {\n    margin: 2px;\n}"), 0);
+            assert.equal(css.indexOf(".e90a9ca9bb862787d5423d08ffa8a320_booga {\n    background: green;\n}"), 63);
+            assert.equal(css.indexOf(".624cf273647af7597f5866483cde95d0_booga {\n    color: red;\n    background: blue;\n}"), 131);
+            assert.equal(css.indexOf(".624cf273647af7597f5866483cde95d0_tooga {\n    border: 1px solid white;\n}"), 218);
+        });
+
+        describe("values", function() {
+            it("should fail if a non-existant import is referenced", function() {
+                assert.throws(function() {
+                    imports.process("./test/specimens/imports/invalid-value.css");
+                });
+            });
+        });
+
+        describe("composition", function() {
+            it("should fail if a non-existant import is referenced", function() {
+                assert.throws(function() {
+                    imports.process("./test/specimens/imports/invalid-composition.css");
+                });
             });
         });
     });
