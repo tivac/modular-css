50897011b904c31cb2d73e86ac0bfce9bced1c7e
Pat Cavit <github@patcavit.com>
2016-07-06 15:39:55 -0700

    Include dependencies in graph
    Mostly so `rollup --watch` monitors the files correctly. Those deps will
    be tree-shaken out anyways since nothing else references them.
    
    Also adds a central place to create path-friendly relative paths,
    because browserify AND rollup doing it in code was stupid.

--- src/lib/relative.js
+++ src/lib/relative.js
@@ -2,10 +2,21 @@
 
 var path = require("path"),
 
-    regex = /\\/g;
+    sepRegex    = /\\/g,
+    prefixRegex = /^\.\.?\//;
 
 // Get a relative version of an absolute path w/ cross-platform/URL-friendly
 // directory separators
 module.exports = function(cwd, file) {
-    return path.relative(cwd, file).replace(regex, "/");
+    return path.relative(cwd, file).replace(sepRegex, "/");
+};
+
+module.exports.prefixed = function(cwd, file) {
+    var out = module.exports(cwd, file);
+
+    if(!prefixRegex.test(out)) {
+        out = "./" + out;
+    }
+
+    return out;
 };
