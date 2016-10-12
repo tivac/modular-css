0cb224d925f7254becdf79c040cffb13d0864232
Pat Cavit <github@patcavit.com>
2016-10-11 23:51:52 -0700

    Only run the test on case-insensitive filesystems

--- test/issue-191.test.js
+++ test/issue-191.test.js
@@ -1,7 +1,7 @@
 "use strict";
 
-var path   = require("path"),
-    assert = require("assert"),
+var fs     = require("fs"),
+    path   = require("path"),
     
     compare = require("./lib/compare-files.js"),
 
@@ -9,8 +9,19 @@ var path   = require("path"),
 
 describe("/issues", () => {
     describe("/191", () => {
+        after((done) => require("rimraf")("./test/output/sensitive.txt", done));
+
         it("should ignore case differences in file paths", () => {
-            var processor = new Processor();
+            var processor;
+            
+            // Verify that filesystem is case-insensitive before bothering
+            fs.writeFileSync("./test/output/sensitive.txt");
+
+            if(!fs.statSync("./test/output/SENSITIVE.txt")) {
+                return this.skip();
+            }
+
+            processor = new Processor();
             
             return processor.file("./test/specimens/issues/191/start.css")
                 .then(() => processor.output())
