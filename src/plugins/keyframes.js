84337556c5e4a03a6410d19a833afb51536882e6
Pat Cavit <github@patcavit.com>
2016-11-02 11:40:07 -0700

    Split apart classes & keyframe messages
    Fixes #208 by not just taking the first match but instead replacing all
    instances of matching keyframe rules.

--- src/plugins/keyframes.js
+++ src/plugins/keyframes.js
@@ -1,27 +1,20 @@
 "use strict";
 
 var postcss = require("postcss"),
-    
+    escape  = require("escape-string-regexp"),
+
     message = require("../lib/message"),
     
     plugin = "postcss-modular-css-keyframes";
 
 module.exports = postcss.plugin(plugin, function() {
     return function(css, result) {
-        var refs  = message(result, "classes"),
-            names = Object.keys(refs);
+        var refs   = message(result, "keyframes"),
+            search = new RegExp(`(\\b${Object.keys(refs).map(escape).join("\\b)|(\\b")}\\b)`, "g");
         
         // Go look up "animation" declarations and rewrite their names to scoped values
         css.walkDecls(/animation$|animation-name$/, function(decl) {
-            names.some(function(name) {
-                if(decl.value.indexOf(name) === -1) {
-                    return false;
-                }
-                
-                decl.value = decl.value.replace(name, refs[name]);
-                
-                return true;
-            });
+            decl.value = decl.value.replace(search, (match) => refs[match]);
         });
     };
 });
