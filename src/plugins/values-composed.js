95d957034211d22c707a8827fed56f28a2f066c7
Pat Cavit <github@patcavit.com>
2016-11-02 22:50:50 -0700

    Value composition plugin + tests

--- src/plugins/values-composed.js
+++ src/plugins/values-composed.js
@@ -1,25 +1,58 @@
 "use strict";
 
-var values      = require("./_values.js"),
-    composition = require("../parsers/composition.js");
+var postcss = require("postcss"),
 
-module.exports = values(
-    "postcss-modular-css-values-composed",
-    function resolveImports(options, rule) {
-        var parsed = composition.rule(options.from, rule),
-            out    = {},
-            source;
-        
-        source = options.files[parsed.source];
+    parser  = require("../parsers/values.js"),
+    resolve = require("../lib/resolve.js"),
+    
+    plugin = "postcss-modular-css-values-composed",
+    offset = "@value ".length;
 
-        parsed.refs.forEach((ref) => {
-            if(!(ref.name in source.values)) {
-                throw rule.error(`Invalid @value reference: ${ref.name}`, { word : ref.name });
-            }
+// Find @value fooga: wooga entries & catalog/remove them
+module.exports = postcss.plugin(plugin, function() {
+    return function(css, result) {
+        var values = Object.create(null);
+
+        css.walkAtRules("value", (rule) => {
+            var parsed, source;
             
-            out[ref.name] = source.values[ref.name];
+            try {
+                 parsed = parser.parse(rule.params);
+            } catch(e) {
+                throw rule.error(e.toString(), { index : offset + e.location.start.column });
+            }
+
+            if(parsed.type !== "composition") {
+                return false;
+            }
+
+            try {
+                source = result.opts.files[resolve(result.opts.from, parsed.source)];
+            } catch(e) {
+                // NO-OP
+            }
+
+            if(!source) {
+                throw rule.error("Unknown composition source", { word : parsed.source });
+            }
+
+            parsed.refs.forEach((ref) => {
+                if(!source.values[ref.name]) {
+                    throw rule.error(`Invalid @value reference: ${ref.name}`, { word : ref.name });
+                }
+                
+                values[ref.name] = source.values[ref.name];
+            });
+
+            return rule.remove();
         });
         
-        return out;
-    }
-);
+        if(Object.keys(values).length > 0) {
+            result.messages.push({
+                type : "modularcss",
+                plugin,
+                values
+            });
+        }
+    };
+});
