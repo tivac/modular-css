73c833f499a12800546136875beb81a5231fcf68
Pat Cavit <github@patcavit.com>
2016-11-03 00:02:47 -0700

    Value replacement re-work + tests
    Most tests are horribly broken though hooray

--- src/plugins/values-replace.js
+++ src/plugins/values-replace.js
@@ -1,11 +1,54 @@
 "use strict";
 
-var postcss   = require("postcss"),
+var postcss = require("postcss"),
+    parser  = require("postcss-value-parser"),
+    each    = require("lodash.foreach"),
 
-    plugin = "postcss-modular-css-replace-values";
+    plugin  = "postcss-modular-css-values-replace",
+    matches = /values-local|values-composed/;
+
+function replacer(values, prop) {
+    return (thing) => {
+        var parsed = parser(thing[prop]);
+        
+        parsed.walk((node) => {
+            if(node.type !== "word" || !values[node.value]) {
+                return;
+            }
+            
+            // Re-assign source first because we're changing the key (node.value) otherwise
+            // Only storing the last source that applied, we can only use one anyways
+            thing.source = values[node.value].source;
+            thing[prop]  = values[node.value].value;
+        });
+    };
+}
 
 module.exports = postcss.plugin(plugin, function() {
     return function(css, result) {
-        // TODO: anything
+        var values = result.messages
+                .filter((msg) => msg.plugin.search(matches) > -1)
+                .reduce((prev, curr) => Object.assign(prev, curr.values), Object.create(null));
+        
+        // Bail if no values
+        if(!Object.keys(values).length) {
+            return;
+        }
+
+        // Walk through all values &  update any inter-dependent values
+        // before replacing anything in the CSS
+        each(values, (details, name) => {
+            parser(details.value).walk((node) => {
+                if(node.type !== "word" || !values[node.value]) {
+                    return;
+                }
+
+                values[name] = values[node.value];
+            });
+        });
+        
+        // Replace all instances of @value keys w/ the value
+        css.walkDecls(replacer(values, "value"));
+        css.walkAtRules(/media|value/, replacer(values, "params"));
     };
 });
--- test/plugin.values-replace.test.js
+++ test/plugin.values-replace.test.js
@@ -1,123 +1,115 @@
 "use strict";
 
-var assert = require("assert"),
+var path   = require("path"),
+    assert = require("assert"),
     
-    plugin = require("../src/plugins/values-local");
+    postcss = require("postcss"),
+
+    plugin     = require("../src/plugins/values-replace.js"),
+    local      = require("../src/plugins/values-local.js"),
+    composed   = require("../src/plugins/values-composed.js"),
+    namespaced = require("../src/plugins/values-namespaced.js");
 
 describe("/plugins", function() {
-    describe("/values-local.js", function() {
+    describe("/values-replace.js", function() {
         var process;
         
-        beforeEach(function() {
-            process = function(css, options) {
-                return plugin.process(css, Object.assign({
-                    files : {
-                        "test.css" : {}
-                    },
-                    from : "test.css"
-                }, options));
-            };
-        });
-        
-        it("should silently ignore invalid declarations", function() {
-            /* eslint no-unused-expressions:0 */
+        describe("local values", function() {
+            var css = postcss([ local, plugin ]);
             
-            assert.doesNotThrow(function() {
-                process("@value green").css;
+            it("should replace values in declarations", function() {
+                assert.equal(
+                    css.process("@value color: red; .wooga { color: color; }").css,
+                    ".wooga { color: red; }"
+                );
             });
-            
-            assert.doesNotThrow(function() {
-                process("@value red:").css;
+
+            it("should replace value references in @value declarations", function() {
+                assert.equal(
+                    css.process("@value color: red; @value value: color; .wooga { color: value; }").css,
+                    ".wooga { color: red; }"
+                );
             });
-            
-            assert.doesNotThrow(function() {
-                process("@value blue red").css;
+
+            it("should replace values in media queries", function() {
+                assert.equal(
+                    css.process("@value small: (max-width: 599px); @media small { }").css,
+                    "@media (max-width: 599px) { }"
+                );
             });
         });
 
-        it("should noop if no @value rules are defined", function() {
-            assert.equal(
-                process(".wooga { color: red; }").css,
-                ".wooga { color: red; }"
-            );
+        describe("composed values", function() {
+            var css = postcss([ composed, plugin ]);
+            
+            it("should replace values in declarations", function() {
+                this.timeout(0);
+                
+                assert.equal(
+                    css.process(`@value color from "./local.css"; .wooga { color: color; }`, {
+                        map   : false,
+                        from  : path.resolve("./test/specimens/in.css"),
+                        files : {
+                            // Composition source
+                            [path.resolve("./test/specimens/in.css")] : {},
+
+                            // Composition target
+                            [path.resolve("./test/specimens/local.css")] : {
+                                values : {
+                                    color : {
+                                        value  : "red",
+                                        source : {}
+                                    }
+                                }
+                            }
+                        }
+                    }).css,
+                    ".wooga { color: red; }"
+                );
+            });
+
+            it("should replace value references in @value declarations", function() {
+                assert.equal(
+                    css.process("@value color: red; @value value: color; .wooga { color: value; }").css,
+                    ".wooga { color: red; }"
+                );
+            });
+
+            it("should replace values in media queries", function() {
+                assert.equal(
+                    css.process("@value small: (max-width: 599px); @media small { }").css,
+                    "@media (max-width: 599px) { }"
+                );
+            });
         });
 
-        it("should replace values in declarations", function() {
-            assert.equal(
-                process("@value color: red; .wooga { color: color; }").css,
-                ".wooga { color: red; }"
-            );
+        describe("namedspaced values", function() {
+            var css = postcss([ namespaced, plugin ]);
+            
+            it("should replace values in declarations", function() {
+                assert.equal(
+                    css.process("@value color: red; .wooga { color: color; }").css,
+                    ".wooga { color: red; }"
+                );
+            });
+
+            it("should replace value references in @value declarations", function() {
+                assert.equal(
+                    css.process("@value color: red; @value value: color; .wooga { color: value; }").css,
+                    ".wooga { color: red; }"
+                );
+            });
+
+            it("should replace values in media queries", function() {
+                assert.equal(
+                    css.process("@value small: (max-width: 599px); @media small { }").css,
+                    "@media (max-width: 599px) { }"
+                );
+            });
         });
 
-        it("should replace value references in @value declarations", function() {
-            assert.equal(
-                process("@value color: red; @value value: color; .wooga { color: value; }").css,
-                ".wooga { color: red; }"
-            );
-        });
 
-        it("should replace values in media queries", function() {
-            assert.equal(
-                process(
-                    "@value small: (max-width: 599px);" +
-                    "@media small { .wooga { color: red; } }" +
-                    "@media (max-width: 799px) { .wooga { color: red; } }"
-                ).css,
-                "@media (max-width: 599px) { .wooga { color: red; } }" +
-                "@media (max-width: 799px) { .wooga { color: red; } }"
-            );
-        });
-
-        it("should support multiple values", function() {
-            assert.equal(
-                process("@value color: red; @value 2color: blue; .wooga { color: color; background: 2color; }").css,
-                ".wooga { color: red; background: blue; }"
-            );
-        });
-
-        it("should support multiple values in a single declaration", function() {
-            assert.equal(
-                process("@value color: red; @value 2color: blue; .wooga { background: linear-gradient(color, 2color); }").css,
-                ".wooga { background: linear-gradient(red, blue); }"
-            );
-        });
-
-        it("should support complex values", function() {
-            assert.equal(
-                process("@value base: 10px; @value large: calc(base * 2); .wooga { margin: large; }").css,
-                ".wooga { margin: calc(10px * 2); }"
-            );
-        });
-        
-        it("should support values containing commas", function() {
-            assert.equal(
-                process("@value fonts: -apple-system, '.SFNSText-Regular', 'San Francisco', 'Oxygen', 'Ubuntu', 'Roboto', 'Segoe UI', 'Helvetica Neue', 'Lucida Grande', sans-serif; .wooga { font-family: fonts; }").css,
-                ".wooga { font-family: -apple-system, '.SFNSText-Regular', 'San Francisco', 'Oxygen', 'Ubuntu', 'Roboto', 'Segoe UI', 'Helvetica Neue', 'Lucida Grande', sans-serif; }"
-            );
-        });
-
-        it("should support value heirarchies", function() {
-            assert.equal(
-                process("@value color: red; @value 2color: color; .wooga { color: 2color; }").css,
-                ".wooga { color: red; }"
-            );
-
-            assert.equal(
-                process("@value color: red; @value 2color: color; @value 3color: 2color; .wooga { color: 3color; }").css,
-                ".wooga { color: red; }"
-            );
-        });
-        
-        it("should output correct sourcemaps", function() {
-            assert.equal(
-                process(
-                    "@value color: red; .wooga { color: color; }",
-                    { map : true, from : "test.css", to : "out.css" }
-                ).css,
-                ".wooga { color: red; }\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInRlc3QuY3NzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFtQixTQUFuQixXQUFrQixFQUF5QiIsImZpbGUiOiJvdXQuY3NzIiwic291cmNlc0NvbnRlbnQiOlsiQHZhbHVlIGNvbG9yOiByZWQ7IC53b29nYSB7IGNvbG9yOiBjb2xvcjsgfSJdfQ== */");
-        });
-
-        it("should write exported values to the files object", function() {
+        it.skip("should write exported values to the files object", function() {
             var files = {
                     "test.css" : {}
                 };
