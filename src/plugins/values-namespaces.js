d55db40ac3ed46679b83ec2b612f53f4cb1698c6
Pat Cavit <github@patcavit.com>
2016-11-02 22:51:20 -0700

    Value namespaces and tests

---
+++ src/plugins/values-namespaces.js
@@ -0,0 +1,52 @@
+"use strict";
+
+var postcss = require("postcss"),
+
+    parser  = require("../parsers/values.js"),
+    resolve = require("../lib/resolve.js"),
+    
+    plugin = "postcss-modular-css-values-namespaces",
+    offset = "@value ".length;
+
+// Find @value fooga: wooga entries & catalog/remove them
+module.exports = postcss.plugin(plugin, function() {
+    return function(css, result) {
+        var values = Object.create(null);
+
+        css.walkAtRules("value", (rule) => {
+            var parsed, source;
+            
+            try {
+                 parsed = parser.parse(rule.params);
+            } catch(e) {
+                throw rule.error(e.toString(), { index : offset + e.location.start.column });
+            }
+
+            if(parsed.type !== "namespace") {
+                return false;
+            }
+
+            try {
+                source = result.opts.files[resolve(result.opts.from, parsed.source)];
+            } catch(e) {
+                // NO-OP
+            }
+
+            if(!source) {
+                throw rule.error("Unknown composition source", { word : parsed.source });
+            }
+
+            values[parsed.name] = source.values;
+
+            return rule.remove();
+        });
+        
+        if(Object.keys(values).length > 0) {
+            result.messages.push({
+                type : "modularcss",
+                plugin,
+                values
+            });
+        }
+    };
+});
---
+++ test/plugin.values-namespaces.test.js
@@ -0,0 +1,84 @@
+"use strict";
+
+var path   = require("path"),
+    assert = require("assert"),
+    
+    plugin = require("../src/plugins/values-namespaces.js");
+
+describe("/plugins", function() {
+    describe("/values-namespaces.js", function() {
+        // Helper to create environment where other files are already processed
+        function process(css) {
+            return plugin.process(css, {
+                from  : path.resolve("./test/specimens/start.css"),
+                files : {
+                    // Composition source
+                    [path.resolve("./test/specimens/start.css")] : {},
+
+                    // Composition targets
+                    [path.resolve("./test/specimens/empty.css")] : {},
+                    [path.resolve("./test/specimens/local.css")] : {
+                        values : {
+                            fooga : {
+                                value  : "red",
+                                source : {}
+                            },
+
+                            googa : {
+                                value  : "blue",
+                                source : {}
+                            }
+                        }
+                    }
+                }
+            });
+        }
+        
+        it("should fail to parse invalid declarations", function() {
+            assert.throws(() => process(`@value * as fooga from "./local.css`).css, /: Unclosed string/);
+            assert.throws(() => process(`@value * as fooga`).css, /SyntaxError: Expected "from"/);
+        });
+
+        it("should fail if importing from a file that doesn't exist", function() {
+            assert.throws(function() {
+                process(`@value * as booga from "./no.css";`).css;
+            }, /Unknown composition source/);
+        });
+
+        it("should fail if importing a file w/ no exports", function() {
+            assert.throws(function() {
+                process(`@value * as booga from "./empty.css";`).css;
+            }, /Unknown composition source/);
+        });
+
+        it("should ignore other @value types", function() {
+            assert.equal(
+                process(`@value fooga from "./local.css"; @value tooga: red;`).css,
+                `@value fooga from "./local.css"; @value tooga: red;`
+            );
+        });
+
+        it("should support importing a value from another file", function() {
+            assert.deepEqual(
+                process(`@value * as ns from "./local.css";`).messages,
+                [{
+                    type   : "modularcss",
+                    plugin : "postcss-modular-css-values-namespaces",
+                    values : {
+                        ns : {
+                            fooga : {
+                                value  : "red",
+                                source : {}
+                            },
+
+                            googa : {
+                                value  : "blue",
+                                source : {}
+                            }
+                        }
+                    }
+                }]
+            );
+        });
+    });
+});
