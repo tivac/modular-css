3ac21e7b24ce0e17c67a2ddb32e2be2011db5647
Pat Cavit <github@patcavit.com>
2016-11-11 14:11:38 -0800

    Add :external() support to graph-nodes scanner

---
+++ test/plugin.graph-nodes.test.js
@@ -0,0 +1,70 @@
+"use strict";
+
+var path   = require("path"),
+    assert = require("assert"),
+
+    postcss = require("postcss"),
+    Graph   = require("dependency-graph").DepGraph,
+    
+    plugin = require("../src/plugins/graph-nodes.js");
+
+describe("/plugins", function() {
+    describe("/graph-nodes.js", function() {
+        it("should populate a graph with external @value references", () => {
+            var graph = new Graph(),
+                from  = path.resolve("./test/specimens/simple.css");
+
+            graph.addNode(from);
+            
+            return plugin.process(
+                `@value one from "./local.css";`,
+                {
+                    from,
+                    graph
+                }
+            )
+            .then(() => assert.deepEqual(graph.overallOrder(), [
+                path.resolve("./test/specimens/local.css"),
+                from
+            ]));
+        });
+
+        it("should populate a graph with external composes references", () => {
+            var graph = new Graph(),
+                from  = path.resolve("./test/specimens/simple.css");
+
+            graph.addNode(from);
+            
+            return plugin.process(
+                `.a { composes: booga from "./local.css"; }`,
+                {
+                    from,
+                    graph
+                }
+            )
+            .then(() => assert.deepEqual(graph.overallOrder(), [
+                path.resolve("./test/specimens/local.css"),
+                from
+            ]));
+        });
+
+        it("should populate a graph with :external references", () => {
+            var graph = new Graph(),
+                from  = path.resolve("./test/specimens/simple.css");
+
+            graph.addNode(from);
+            
+            return plugin.process(
+                `.a :external(booga from "./local.css") { color: red; }`,
+                {
+                    from,
+                    graph
+                }
+            )
+            .then(() => assert.deepEqual(graph.overallOrder(), [
+                path.resolve("./test/specimens/local.css"),
+                from
+            ]));
+        });
+    });
+});
