fb3bf0f5dd3dde0cfb60aabaf7fb33601d3ae3c8
Pat Cavit <github@patcavit.com>
2016-11-14 17:14:12 -0800

    Starting on webpack support
    So, SO broken atm.

---
+++ test/webpack.test.js
@@ -0,0 +1,37 @@
+"use strict";
+
+var path   = require("path"),
+    assert = require("assert"),
+    
+    webpack = require("webpack"),
+    
+    loader  = require("../src/webpack.js"),
+    compare = require("./lib/compare-files.js");
+
+describe.only("/webpack.js", function() {
+    it("should be a function", function() {
+        assert(typeof loader, "function")
+    });
+
+    it("should do something", function(done) {
+        webpack({
+            entry   : "./test/specimens/simple.js",
+            output  : {
+                path : path.resolve("./test/results/webpack"),
+                filename : "./simple.js"
+            },
+            module : {
+                loaders : [
+                    {
+                        test   : /\.css$/,
+                        loader : "css-loader" //path.resolve("./src/webpack.js")
+                    }
+                ]
+            }
+        }, (err, stats) => {
+            assert.ifError(err);
+
+            done();
+        });
+    });
+});
