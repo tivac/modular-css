cdabdd529dd0b4a87d67da0fc79f76d1da4eb05a
Norbert de Langen <ndelangen@me.com>
2016-11-18 13:42:45 +0100

    CHANGE `selector` to `postcssParser`, because of `selector.selector()`

--- src/plugins/externals.js
+++ src/plugins/externals.js
@@ -1,11 +1,11 @@
 "use strict";
 
 var postcss  = require("postcss"),
-    selector = require("postcss-selector-parser"),
+    postcssParser = require("postcss-selector-parser"),
 
     parser  = require("../parsers/parser.js"),
     resolve = require("../lib/resolve.js"),
-    
+
     plugin = "postcss-modular-css-externals";
 
 // Find :external(<rule> from <file>) references and update them to be
@@ -13,55 +13,54 @@ var postcss  = require("postcss"),
 module.exports = postcss.plugin(plugin, function() {
     return function(css, result) {
         css.walkRules(/:external/, (rule) => {
-            var externals = selector((selectors) =>
-                    selectors.walkPseudos((pseudo) => {
-                        var params = pseudo.nodes.toString(),
-                            parsed = parser.parse(params),
-                            root   = selector.root(),
-                            source;
+            var externals = postcssParser((selectors) =>
+                selectors.walkPseudos((pseudo) => {
+                    var params = pseudo.nodes.toString(),
+                        parsed = parser.parse(params),
+                        root   = postcssParser.root(),
+                        source;
 
-                        if(parsed.type !== "composition") {
-                            throw rule.error(
-                                "externals must be from another file",
-                                { word : params }
-                            );
+                    if(parsed.type !== "composition") {
+                        throw rule.error(
+                            "externals must be from another file",
+                            { word : params }
+                        );
+                    }
+
+                    try {
+                        source = result.opts.files[resolve(result.opts.from, parsed.source)];
+                    } catch(e) {
+                        // NO-OP
+                    }
+
+                    if(!source) {
+                        throw rule.error(
+                            "Unknown external source",
+                            { word : parsed.source }
+                        );
+                    }
+
+                    // There will only ever be one, but this is nicer
+                    parsed.refs.forEach((ref) => {
+                        var s;
+
+                        if(!source.exports[ref.name]) {
+                            throw rule.error(`Invalid external reference: ${ref.name}`, { word : ref.name });
                         }
 
-                        try {
-                            source = result.opts.files[resolve(result.opts.from, parsed.source)];
-                        } catch(e) {
-                            // NO-OP
-                        }
+                        s = postcssParser.selector();
 
-                        if(!source) {
-                            throw rule.error(
-                                "Unknown external source",
-                                { word : parsed.source }
-                            );
-                        }
+                        source.exports[ref.name].forEach((name) =>
+                            s.append(postcssParser.className({ value : name }))
+                        );
 
-                        // There will only ever be one, but this is nicer
-                        parsed.refs.forEach((ref) => {
-                            var s;
-                            
-                            if(!source.exports[ref.name]) {
-                                throw rule.error(`Invalid external reference: ${ref.name}`, { word : ref.name });
-                            }
+                        root.append(s);
+                    });
 
-                            // This was a... poor naming choice
-                            s = selector.selector();
+                    pseudo.replaceWith(root);
+                })
+            );
 
-                            source.exports[ref.name].forEach((name) =>
-                                s.append(selector.className({ value : name }))
-                            );
-                            
-                            root.append(s);
-                        });
-
-                        pseudo.replaceWith(root);
-                    })
-                );
-            
             rule.selector = externals.process(rule.selector).result;
         });
     };
