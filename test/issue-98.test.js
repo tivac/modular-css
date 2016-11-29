96b926ee81a4a950f4022f163f6ad38a93ee6e1e
Pat Cavit <github@patcavit.com>
2016-11-28 16:48:33 -0800

    Clean up #98 support some more

--- test/issue-98.test.js
+++ test/issue-98.test.js
@@ -11,16 +11,16 @@ describe("/issues", function() {
     describe("/98", function() {
         it("should prune rules that only compose, but leave them in the exports", function() {
             return plugin.process(
-                require("fs").readFileSync("./test/specimens/issues/98/1.css", "utf8"),
-                { from : "./test/specimens/issues/98/1.css" }
+                require("fs").readFileSync("./test/specimens/issues/98.css", "utf8"),
+                { from : "./test/specimens/issues/98.css" }
             )
             .then((result) => {
                 assert.deepEqual(
                     compositions(result),
                     {
-                        "test/specimens/issues/98/1.css" : {
-                            booga : "mc8809d089_booga",
-                            fooga : "mc8809d089_booga mc8809d089_fooga"
+                        "test/specimens/issues/98.css" : {
+                            booga : "mc2a6c9ee9_booga",
+                            fooga : "mc2a6c9ee9_booga mc2a6c9ee9_fooga"
                         }
                     }
                 );
