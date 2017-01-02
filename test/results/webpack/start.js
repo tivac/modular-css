ab97e302da0fb106f056825007b6014d4a5c5431
Pat Cavit <github@patcavit.com>
2017-01-02 14:17:40 -0800

    test: webpack tests & specimens/results

--- test/results/webpack/start.js
+++ test/results/webpack/start.js
@@ -71,7 +71,9 @@
 /***/ function(module, exports) {
 
 module.exports = {
-    "wooga": "mc08e91a5b_wooga"
+    "wooga": "mc04cb4cb2_booga mc61f0515a_wooga",
+    "booga": "mc61f0515a_booga",
+    "tooga": "mc61f0515a_tooga"
 };
 
 /***/ },
--- test/webpack.test.js
+++ test/webpack.test.js
@@ -5,33 +5,141 @@ var path   = require("path"),
 
     webpack = require("webpack"),
 
-    loader  = require("../src/webpack.js"),
-    compare = require("./lib/compare-files.js");
+    Plugin  = require("../src/webpack.js"),
+    compare = require("./lib/compare.js");
 
-describe.only("/webpack.js", function() {
+describe("/webpack.js", function() {
+    after(() => require("shelljs").rm("-rf", "./test/output/webpack"));
+
     it("should be a function", function() {
-        assert(typeof loader, "function")
+        assert(typeof Plugin, "function")
     });
 
-    it("should do something", function(done) {
+    it("should output css to disk", function(done) {
+        var plugin = new Plugin({
+                css : "./test/output/webpack/simple.css"
+            });
+
         webpack({
             entry   : "./test/specimens/simple.js",
             output  : {
-                path : path.resolve("./test/results/webpack"),
+                path : path.resolve("./test/output/webpack"),
                 filename : "./simple.js"
             },
             module : {
                 rules : [
-                    {
-                        test   : /\.css$/,
-                        loader : "./src/webpack"
-                    }
+                    plugin.rule({
+                        test : /\.css$/
+                    })
                 ]
-            }
+            },
+            plugins : [
+                plugin
+            ]
         }, (err, stats) => {
             assert.ifError(err);
+            assert.equal(stats.hasErrors(), false);
+
+            compare.results("webpack/simple.js");
+            compare.results("webpack/simple.css");
 
             done();
         });
     });
+
+    it("should output json to disk", function(done) {
+        var plugin = new Plugin({
+                json : "./test/output/webpack/simple.json"
+            });
+
+        webpack({
+            entry   : "./test/specimens/simple.js",
+            output  : {
+                path : path.resolve("./test/output/webpack"),
+                filename : "./simple.js"
+            },
+            module : {
+                rules : [
+                    plugin.rule({
+                        test : /\.css$/
+                    })
+                ]
+            },
+            plugins : [
+                plugin
+            ]
+        }, (err, stats) => {
+            assert.ifError(err);
+            assert.equal(stats.hasErrors(), false);
+
+            compare.results("webpack/simple.js");
+            compare.results("webpack/simple.json");
+
+            done();
+        });
+    });
+
+    it("should report errors", function(done) {
+        var plugin = new Plugin();
+
+        webpack({
+            entry   : "./test/specimens/invalid.js",
+            output  : {
+                path : path.resolve("./test/output/webpack"),
+                filename : "./invalid.js"
+            },
+            module : {
+                rules : [
+                    plugin.rule({
+                        test : /\.css$/
+                    })
+                ]
+            },
+            plugins : [
+                plugin
+            ]
+        }, (err, stats) => {
+            assert.ifError(err);
+
+            assert.equal(stats.hasErrors(), true);
+
+            assert(stats.toJson().errors[0].indexOf("Invalid composes reference") > -1);
+
+            done();
+        });
+    });
+
+    it("should handle dependencies", function(done) {
+        var plugin = new Plugin({
+                css  : "./test/output/webpack/start.css",
+                json : "./test/output/webpack/start.json"
+            });
+
+        webpack({
+            entry   : "./test/specimens/start.js",
+            output  : {
+                path : path.resolve("./test/output/webpack"),
+                filename : "./start.js"
+            },
+            module : {
+                rules : [
+                    plugin.rule({
+                        test : /\.css$/
+                    })
+                ]
+            },
+            plugins : [
+                plugin
+            ]
+        }, (err, stats) => {
+            assert.ifError(err);
+            assert.equal(stats.hasErrors(), false);
+
+            compare.results("webpack/start.js");
+            compare.results("webpack/start.css");
+            compare.results("webpack/start.json");
+
+            done();
+        });
+    })
 });
