0be701d01f49c9e9fbca4ebf09ac22ab370e0e27
Pat Cavit <github@patcavit.com>
2017-02-28 00:02:37 -0800

    feat: world's dumbest repl

--- packages/www/index.js
+++ packages/www/index.js
@@ -1,7 +1,55 @@
-import Processor from "modular-css-core";
+var m = require("mithril");
+var Processor = require("modular-css-core");
+var cm = require("codemirror");
+var prism = require("prismjs");
 
-var processor = new Processor();
+var files = [
+    { name : "test.css", css : "" }
+];
+var output = "";
 
-processor.string("/test.css", ".fooga { animation-name: none; }")
+
+function process() {
+    var processor = new Processor();
+    
+    Promise.all(files.map((file) => processor.string(file.name, file.css)))
     .then(() => processor.output())
-    .then(console.log.bind(console));
+    .then((result) => {
+        output = result.css;
+
+        m.redraw();
+    })
+    .catch(console.error.bind(console));
+}
+
+m.mount(document.body, {
+    view : () => m("div",
+        files.map((file) => m("div",
+            m("input", { value : file.name }),
+            m("textarea", {
+                    oncreate : (vnode) => {
+                        var editor = cm.fromTextArea(vnode.dom);
+
+                        editor.on("changes", () => {
+                            file.css = editor.doc.getValue();
+                            
+                            process();
+                        });
+                    }
+                },
+                file.css
+            )
+        )),
+
+        m("button", {
+            onclick : (e) => {
+                files.push({
+                    name : Date.now(),
+                    contents : ""
+                })
+            }
+        }, "Add file"),
+
+        m("pre", m("code", { class : "language-css" }, output))
+    )
+})
