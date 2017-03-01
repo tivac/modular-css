cf1e444919d0647575ef76be1f5f893bf0af8e6a
Pat Cavit <github@patcavit.com>
2017-03-01 00:06:15 -0800

    feat: shareable URLs, file removal
    Picked up a nasty prism redrawing bug somewhere along the way though...

--- packages/www/index.js
+++ packages/www/index.js
@@ -16,6 +16,7 @@ var m = require("mithril"),
     error,
     throttled;
 
+// Hacks to get file resolution to work
 require("module")._nodeModulePaths = () => [ "/" ];
 require("module")._resolveFilename = (id) => id.slice(1);
 
@@ -30,6 +31,8 @@ function process() {
         output.css = result.css;
         output.js  = JSON.stringify(result.compositions, null, 4);
         
+        m.route.set(`/?${m.buildQueryString({ state : btoa(JSON.stringify(files)) })}`);
+
         error = false;
     })
     .catch((e) => (error = e.toString()))
@@ -38,54 +41,95 @@ function process() {
 
 throttled = throttle(process, 200);
 
-m.mount(document.body, {
-    view : () => m("div", { class : css.content },
-        m("div", { class : css.files },
-            m("button", {
-                class : css.add,
+m.route(document.body, "/", {
+    "/" : {
+        oninit : (vnode) => {
+            if(!vnode.attrs.state) {
+                return;
+            }
+            
+            try {
+                files = JSON.parse(atob(vnode.attrs.state));
 
-                onclick : () => files.push({
-                    name : `${files.length + 1}.css`,
-                    css  : ""
-                })
-            }, "Add file"),
+                process();
+            } catch(e) {
+                error = "Unable to parse saved state";
+            }
+        },
 
-            files.map((file) => m("div", { class : css.file },
-                m("input", {
-                    class    : css.name,
-                    value    : file.name,
-                    onchange : (e) => (file.name = e.target.value)
-                }),
+        view : () => m("div", { class : css.content },
+            m("div", { class : css.files },
+                m("button", {
+                    class : css.add,
+
+                    onclick : () => files.push({
+                        name : `${files.length + 1}.css`,
+                        css  : ""
+                    })
+                }, "Add file"),
+
+                files.map((file, idx) => m("div", { class : css.file },
+                    m("div", { class : css.meta },
+                        m("input", {
+                            class    : css.name,
+                            value    : file.name,
+                            onchange : (e) => (file.name = e.target.value)
+                        }),
+
+                        idx > 0 && m("button", {
+                            class : css.remove,
+
+                            onclick : () => {
+                                files.splice(idx, 1);
+
+                                process();
+                            }
+                        }, "✖")
+                    ),
+                    
+                    m("textarea", {
+                            oncreate : (vnode) => {
+                                var editor = cm.fromTextArea(vnode.dom, {
+                                        mode        : "css",
+                                        theme       : "monokai",
+                                        lineNumbers : true,
+                                        autofocus   : true
+                                    });
+
+                                editor.on("changes", () => {
+                                    file.css = editor.doc.getValue();
+                                    
+                                    throttled();
+                                });
+                            }
+                        },
+                        file.css
+                    )
+                ))
+            ),
+
+            m("div", { class : css.output },
+                error && [
+                    m("h2", "Error"),
+                    m("div", { class : css.error },
+                        m("pre", error)
+                    )
+                ],
                 
-                m("textarea", {
-                        oncreate : (vnode) => {
-                            var editor = cm.fromTextArea(vnode.dom);
-
-                            editor.on("changes", () => {
-                                file.css = editor.doc.getValue();
-                                
-                                throttled();
-                            });
-                        }
-                    },
-                    file.css
+                m("h2", "CSS Output"),
+                m("pre.line-numbers",
+                    m("code", { class : "language-css" },
+                        output.css || "/* Nothing yet... */"
+                    )
+                ),
+                
+                m("h2", "JSON Output"),
+                m("pre.line-numbers",
+                    m("code", { class : "language-js" },
+                        output.js || "// Nothing yet..."
+                    )
                 )
-            ))
-        ),
-
-        m("div", { class : css.output },
-            error && [
-                m("h2", "Error"),
-                m("div", { class : css.error },
-                    m("pre", error)
-                )
-            ],
-            
-            m("h2", "CSS Output"),
-            m("pre", m("code", { class : "language-css" }, output.css)),
-            
-            m("h2", "JSON Output"),
-            m("pre", m("code", { class : "language-json" }, output.js))
+            )
         )
-    )
+    }
 });
