577f7fc304b9c4d6f0a38008b26b14c06f7dcc2c
Pat Cavit <github@patcavit.com>
2017-02-28 23:05:36 -0800

    feat: styling and some workflow improvements

--- packages/www/index.js
+++ packages/www/index.js
@@ -1,15 +1,17 @@
-var m = require("mithril");
-var Processor = require("modular-css-core");
-var cm = require("codemirror");
-var prism = require("prismjs");
+/* eslint indent: off */
+var m = require("mithril"),
+    Processor = require("modular-css-core"),
+    cm = require("codemirror"),
+    throttle = require("throttleit"),
 
-var css = require("./style.css");
+    css = require("./style.css"),
 
-var files = [
-    { name : "test.css", css : "" }
-];
-var output = "";
-
+    files = [
+        { name : "test.css", css : "" }
+    ],
+    output = "",
+    error,
+    throttled;
 
 function process() {
     var processor = new Processor();
@@ -18,40 +20,52 @@ function process() {
     .then(() => processor.output())
     .then((result) => {
         output = result.css;
-
-        m.redraw();
+        error = false;
     })
-    .catch(console.error.bind(console));
+    .catch((e) => (error = e.toString()))
+    .then(m.redraw);
 }
 
+throttled = throttle(process, 200);
+
 m.mount(document.body, {
-    view : () => m("div",
-        files.map((file) => m("div",
-            m("input", { value : file.name }),
-            m("textarea", {
-                    oncreate : (vnode) => {
-                        var editor = cm.fromTextArea(vnode.dom);
+    view : () => m("div", { class : css.content },
+        m("div", { class : css.files },
+            files.map((file) => m("div", { class : css.file },
+                m("input", {
+                    class    : css.name,
+                    value    : file.name,
+                    onchange : (e) => (file.name = e.target.value)
+                }),
+                
+                m("textarea", {
+                        oncreate : (vnode) => {
+                            var editor = cm.fromTextArea(vnode.dom);
 
-                        editor.on("changes", () => {
-                            file.css = editor.doc.getValue();
-                            
-                            process();
-                        });
-                    }
-                },
-                file.css
-            )
-        )),
+                            editor.on("changes", () => {
+                                file.css = editor.doc.getValue();
+                                
+                                throttled();
+                            });
+                        }
+                    },
+                    file.css
+                )
+            )),
 
-        m("button", {
-            onclick : (e) => {
-                files.push({
-                    name : Date.now(),
-                    contents : ""
+            m("button", {
+                onclick : () => files.push({
+                    name : `file-${files.length}.css`,
+                    css  : ""
                 })
-            }
-        }, "Add file"),
+            }, "Add file")
+        ),
 
-        m("pre", m("code", { class : "language-css" }, output))
+        m("div", { class : css.output },
+            error && m("div", { class : css.error },
+                m("pre", error)
+            ),
+            m("pre", m("code", { class : "language-css" }, output))
+        )
     )
-})
+});
