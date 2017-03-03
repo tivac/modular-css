5f04cc22e9b0d6e69512b815560094f520683df8
Pat Cavit <github@patcavit.com>
2017-03-02 16:05:33 -0800

    refactor: postcss tests passing via jest

--- packages/postcss/test/postcss.test.js
+++ packages/postcss/test/postcss.test.js
@@ -5,7 +5,9 @@ var fs     = require("fs"),
     assert = require("assert"),
     
     postcss = require("postcss"),
-    compare = require("test-utils/compare.js"),
+    compare = require("test-utils/compare.js")(__dirname),
+    namer   = require("test-utils/namer.js"),
+    
     message = require("modular-css-core/lib/message.js"),
 
     plugin  = require("../postcss.js");
@@ -16,7 +18,8 @@ function process(file, opts) {
         Object.assign(
             Object.create(null),
             {
-                from : file
+                from : file,
+                namer
             },
             opts || {}
         )
@@ -24,51 +27,66 @@ function process(file, opts) {
 }
 
 describe("/postcss.js", function() {
-    after(() => require("shelljs").rm("-rf", "./test/output/postcss"));
+    afterAll(() => require("shelljs").rm("-rf", "./packages/postcss/test/output/postcss"));
     
     it("should be a function", function() {
-        assert.equal(typeof plugin, "function");
+        expect(typeof plugin).toBe("function");
     });
 
     it("should process CSS and output the result", function() {
-        return process("./test/specimens/simple.css")
-            .then((result) => compare.stringToFile(result.css, "./test/results/postcss/simple.css"));
+        return process("./packages/postcss/test/specimens/simple.css")
+            .then((result) =>
+                compare.stringToFile(
+                    result.css,
+                    "./packages/postcss/test/results/postcss/simple.css"
+                )
+            );
     });
 
     it("should process CSS with dependencies and output the result", function() {
-        return process("./test/specimens/start.css")
-            .then((result) => compare.stringToFile(result.css, "./test/results/postcss/start.css"));
+        return process("./packages/postcss/test/specimens/start.css")
+            .then((result) =>
+                compare.stringToFile(
+                    result.css,
+                    "./packages/postcss/test/results/postcss/start.css"
+                )
+            );
     });
 
     it("should process CSS and output exports as a message", function() {
-        return process("./test/specimens/simple.css")
+        return process("./packages/postcss/test/specimens/simple.css")
             .then((result) => assert.deepEqual(
                 message(result, (msg) => msg.type === "modular-css-exports", "exports"),
                 {
-                    wooga : [ "mc08e91a5b_wooga" ]
+                    wooga : [ "wooga" ]
                 }
             ));
     });
 
     it("should accept normal processor options", function() {
-        return process("./test/specimens/simple.css", {
+        return process("./packages/postcss/test/specimens/simple.css", {
             map   : {
                 inline : true
             },
             namer : (f, s) => `fooga_${s}`
         })
-        .then((result) => compare.stringToFile(result.css, "./test/results/postcss/simple-namer.css"));
+        .then((result) =>
+            compare.stringToFile(
+                result.css,
+                "./packages/postcss/test/results/postcss/simple-namer.css"
+            )
+        );
     });
 
     it("should accept a `json` property and write exports to that file", function() {
-        return process("./test/specimens/start.css", { json : "./test/output/postcss/classes.json" })
+        return process("./packages/postcss/test/specimens/start.css", { json : "./packages/postcss/test/output/postcss/classes.json" })
             .then(() => compare.results("postcss/classes.json"));
     });
 
     it("should be usable like a normal postcss plugin", function() {
         var processor = postcss([
                 plugin({
-                    json  : "./test/output/postcss/classes.json",
+                    json  : "./packages/postcss/test/output/postcss/classes.json",
                     map   : {
                         inline : true
                     },
@@ -76,7 +94,12 @@ describe("/postcss.js", function() {
                 })
             ]);
         
-        processor.process(fs.readFileSync("./test/specimens/start.css"), { from : "./test/specimens/start.css" })
-            .then((result) => compare.stringToFile(resut.css, "./test/results/postcss/simple-namer.cs"));
+        processor.process(fs.readFileSync("./packages/postcss/test/specimens/start.css"), { from : "./packages/postcss/test/specimens/start.css" })
+            .then((result) =>
+                compare.stringToFile(
+                    resut.css,
+                    "./packages/postcss/test/results/postcss/simple-namer.cs"
+                )
+            );
     });
 });
