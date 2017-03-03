5f8c4ee79804bcef6d37b124851a9ca96d88c0e5
Pat Cavit <github@patcavit.com>
2017-03-02 16:33:13 -0800

    refactor: flatten CLI tests, clean metadata

--- packages/cli/test/cli.test.js
+++ packages/cli/test/cli.test.js
@@ -11,44 +11,44 @@ var fs     = require("fs"),
     cli = require.resolve("../cli.js");
 
 function success(out) {
-    assert.equal(out.code, 0, out.stderr);
+    expect(out.code).toBe(0);
 
     return out;
 }
 
 describe("/cli.js", function() {
-    afterAll(() => require("shelljs").rm("-rf", "./packages/cli/test/output/cli"));
+    afterAll(() => require("shelljs").rm("-rf", "./packages/cli/test/output"));
 
     it("should show help with no args", function() {
         return tester(cli)
             .then(success)
-            .then((out) => assert(out.stdout.indexOf("--help") > -1));
+            .then((out) => expect(out.stdout).toMatch("--help"));
     });
 
     it("should default to outputting to stdout", function() {
         return tester(cli, "./packages/cli/test/specimens/simple.css")
             .then(success)
-            .then((out) => compare.stringToFile(out.stdout, "./packages/cli/test/results/cli/simple.css"));
+            .then((out) => compare.stringToFile(out.stdout, "./packages/cli/test/results/simple.css"));
     });
 
     it("should support outputting to a file", function() {
-        return tester(cli, "--out=./packages/cli/test/output/cli/simple.css", "./packages/cli/test/specimens/simple.css")
+        return tester(cli, "--out=./packages/cli/test/output/simple.css", "./packages/cli/test/specimens/simple.css")
             .then(success)
-            .then((out) => compare.results("cli/simple.css"));
+            .then((out) => compare.results("simple.css"));
     });
 
     it("should support outputting compositions to a file", function() {
-        return tester(cli, "--json=./packages/cli/test/output/cli/classes.json", "./packages/cli/test/specimens/simple.css")
+        return tester(cli, "--json=./packages/cli/test/output/classes.json", "./packages/cli/test/specimens/simple.css")
             .then(success)
-            .then((out) => compare.results("cli/classes.json"));
+            .then((out) => compare.results("classes.json"));
     });
 
     it("should return the correct error code on invalid CSS", function() {
         return tester(cli, "./packages/cli/test/specimens/invalid.css")
             .then((out) => {
-                assert.equal(out.code, 1);
+                expect(out.code).toBe(1);
                 
-                assert(out.stderr.indexOf("Invalid composes reference") > -1);
+                expect(out.stderr).toMatch("Invalid composes reference");
             });
     });
 });
