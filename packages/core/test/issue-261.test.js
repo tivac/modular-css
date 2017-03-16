66b578e3e9719c66d023e009c0a6e59d02fc96bc
Pat Cavit <github@patcavit.com>
2017-03-15 20:53:21 -0700

    test: adjust some tests after rebase

--- packages/core/test/issue-261.test.js
+++ packages/core/test/issue-261.test.js
@@ -1,20 +1,15 @@
 "use strict";
 
-var assert    = require("assert"),
-
-    leading = require("dentist").dedent,
-    
-    Processor = require("../processor.js"),
-    compare   = require("./lib/compare.js");
+var Processor = require("../processor.js");
 
 describe("/issues", function() {
     describe("/261", function() {
-        it("should allow colons in rules also use :external()", function() {
+        it("should allow colons in rules that also use :external()", function() {
             var processor = new Processor();
             
-            return processor.file("./test/specimens/issues/261/2.css")
-            .then((result) => processor.output())
-            .then((result) => compare.stringToFile(result.css, "./test/results/issues/261.css"));
+            return processor.file("./packages/core/test/specimens/issues/261/2.css")
+            .then(() => processor.output())
+            .then((result) => expect(result.css).toMatchSnapshot());
         });
     });
 });
