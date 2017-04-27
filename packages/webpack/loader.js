59df1960c8d64ceb771a635765bebca4fc905b01
Pat Cavit <github@patcavit.com>
2017-04-26 22:20:44 -0700

    feat: create ES2015 module exports
    Fixes #280, though just exports an empty object if there's no classes in
    the file.

--- packages/webpack/loader.js
+++ packages/webpack/loader.js
@@ -1,6 +1,11 @@
 "use strict";
 
-var output = require("modular-css-core/lib/output.js");
+var path = require("path"),
+
+    keyword = require("esutils").keyword,
+    
+    output   = require("modular-css-core/lib/output.js"),
+    relative = require("modular-css-core/lib/relative.js");
 
 module.exports = function(source) {
     var done      = this.async(),
@@ -10,11 +15,30 @@ module.exports = function(source) {
 
     return processor.string(this.resourcePath, source)
         .then((result) => {
-            processor.dependencies(this.resourcePath).forEach((dep) => this.addDependency(dep));
+            var classes = output.join(result.exports),
+                deps    = processor.dependencies(this.resourcePath),
+                imports = deps.map((file) =>
+                    `import "${relative.prefixed(path.dirname(this.resourcePath), file)}";`
+                );
+
+            deps.forEach((dep) => this.addDependency(dep));
             
             return done(
                 null,
-                `module.exports = ${JSON.stringify(output.join(result.exports), null, 4)};`
+                (imports.length ? `${imports.join("\n")}\n` : "") +
+                Object.keys(classes).reduce(
+                    (prev, curr) => {
+                        // Warn if any of the exported CSS wasn't able to be used as a valid JS identifier
+                        if(keyword.isReservedWordES6(curr) || !keyword.isIdentifierNameES6(curr)) {
+                            this.emitWarning(new Error(`Invalid JS identifier "${curr}", unable to export`));
+                            
+                            return prev;
+                        }
+                        
+                        return `export var ${curr} = "${classes[curr]}";\n${prev}`;
+                    },
+                    `export default ${JSON.stringify(classes, null, 4)};\n`
+                )
             );
         })
         .catch(done);
