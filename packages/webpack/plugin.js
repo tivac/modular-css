d5b47e0183eb52b64b8d31f7730d8c68bb729513
Bogdan Chadkin <trysound@yandex.ru>
2017-05-07 15:32:16 +0300

    fix: webpack incremental build

--- packages/webpack/plugin.js
+++ packages/webpack/plugin.js
@@ -3,20 +3,41 @@
 var sources   = require("webpack-sources"),
     Processor = require("modular-css-core");
 
+function getChangedFiles(prev, timestamps) {
+    var changedFiles,
+        removedFiles;
+
+    changedFiles = Object.keys(timestamps)
+        .filter((file) => !prev[file] || prev[file] < timestamps[file]);
+    removedFiles = Object.keys(prev)
+        .filter((file) => !timestamps[file]);
+
+    return changedFiles.concat(removedFiles);
+}
+
 function ModularCSS(args) {
     var options = Object.assign(
             Object.create(null),
             args
         );
 
+    this.prevTimestamps = {};
+    this.processor = new Processor(options);
     this.options = options;
 }
 
 ModularCSS.prototype.apply = function(compiler) {
-    var processor;
-    
+    compiler.plugin("this-compilation", (compilation) => {
+        var changedFiles = getChangedFiles(this.prevTimestamps, compilation.fileTimestamps);
+
+        this.prevTimestamps = compilation.fileTimestamps;
+        this.processor.remove(changedFiles);
+        // Make our processor instance available to the loader
+        compilation.options.processor = this.processor;
+    });
+
     compiler.plugin("emit", (compilation, done) => {
-        processor.output().then((data) => {
+        this.processor.output().then((data) => {
             if(this.options.css) {
                 compilation.assets[this.options.css] = new sources.RawSource(data.css);
             }
@@ -27,18 +48,8 @@ ModularCSS.prototype.apply = function(compiler) {
                 );
             }
 
-            processor = null;
-
             done();
         });
-    });
-    
-    compiler.plugin("this-compilation", (compilation) => {
-        // Avoid caching issues by dumping the entire processor instance on each run
-        processor = new Processor(this.options);
-        
-        // Make our processor instance available to the loader
-        compilation.options.processor = processor;
     });
 };
 
