d994a1df87cbff4c2dab86ac3b9ed48edb43ad3d
Pat Cavit <github@patcavit.com>
2017-05-09 22:26:21 -0700

    style: match repo style

--- packages/webpack/plugin.js
+++ packages/webpack/plugin.js
@@ -3,16 +3,15 @@
 var sources   = require("webpack-sources"),
     Processor = require("modular-css-core");
 
-function getChangedFiles(prev, timestamps) {
-    var changedFiles,
-        removedFiles;
-
-    changedFiles = Object.keys(timestamps)
-        .filter((file) => !prev[file] || prev[file] < timestamps[file]);
-    removedFiles = Object.keys(prev)
-        .filter((file) => !timestamps[file]);
-
-    return changedFiles.concat(removedFiles);
+// Return a list of changed/removed files based on timestamp objects
+function getChangedFiles(prev, curr) {
+    return Object.keys(curr)
+        .filter((file) =>
+            !prev[file] || prev[file] < curr[file]
+        )
+        .concat(
+            Object.keys(prev).filter((file) => !curr[file])
+        );
 }
 
 function ModularCSS(args) {
@@ -21,25 +20,30 @@ function ModularCSS(args) {
             args
         );
 
-    this.prevTimestamps = {};
+    this.prev = {};
     this.processor = new Processor(options);
     this.options = options;
 }
 
 ModularCSS.prototype.apply = function(compiler) {
     compiler.plugin("this-compilation", (compilation) => {
-        var changedFiles = getChangedFiles(this.prevTimestamps, compilation.fileTimestamps);
+        var files = getChangedFiles(this.prev, compilation.fileTimestamps);
 
-        this.prevTimestamps = compilation.fileTimestamps;
-        this.processor.remove(changedFiles);
-        // Make our processor instance available to the loader
+        // Remove changed/removed files from processor instance
+        this.processor.remove(files);
+        
+        this.prev = compilation.fileTimestamps;
+
+        // Make processor instance available to the loader
         compilation.options.processor = this.processor;
     });
 
     compiler.plugin("emit", (compilation, done) => {
         this.processor.output().then((data) => {
             if(this.options.css) {
-                compilation.assets[this.options.css] = new sources.RawSource(data.css);
+                compilation.assets[this.options.css] = new sources.RawSource(
+                    data.css
+                );
             }
             
             if(this.options.json) {
