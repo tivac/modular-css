a6bd9759547cb981715a9d0427262fcad59016d8
Pat Cavit <github@patcavit.com>
2017-05-11 12:54:20 -0700

    refactor: remove a bunch of unused behavior

--- packages/core/lib/identifiers.js
+++ packages/core/lib/identifiers.js
@@ -1,34 +1,16 @@
 "use strict";
 
-var createParser = require("postcss-selector-parser"),
-    
-    keyframes = /keyframes$/i;
+var createParser = require("postcss-selector-parser");
 
-exports.keyframes = keyframes;
+exports.keyframes = /keyframes$/i;
 
-// TODO: this is probably inefficient, but oh well for now
+// Find all classes that comprise a selector and return 'em
 exports.parse = (selector) => {
     var values = [],
         parser;
     
     parser = createParser((selectors) => {
-        // Walk classes
         selectors.walkClasses((part) => values.push(part.value));
-        
-        // Walk IDs
-        selectors.walkIds((part) => values.push(part.value));
-        
-        // Walk @keyframes definitions
-        selectors.walkTags((part) => {
-            // This is a slightly ridiculous conditional, but postcss-selector-parser
-            // spits out @keyframes <name> as [ @keyframes, <name> ] so we have to do
-            // this flopping around to find the real value. Blech.
-            if(part.parent.nodes[0] &&
-               part.parent.nodes[0] !== part  &&
-               part.parent.nodes[0].value.search(keyframes) > -1) {
-                values.push(part.value);
-            }
-        });
     });
     
     parser.process(selector);
