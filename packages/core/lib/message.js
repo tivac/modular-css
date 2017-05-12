a03d5b4e4e7b8a2e4ffb0fad171ca73ddec111c6
Pat Cavit <github@patcavit.com>
2017-05-11 21:10:38 -0700

    refactor: remove unused functionality

--- packages/core/lib/message.js
+++ packages/core/lib/message.js
@@ -2,16 +2,12 @@
 
 var find = require("lodash.findlast");
 
-module.exports = (results, filter, field) => {
+module.exports = (results, filter) => {
     var message = find(results.messages, filter);
 
-    if(!field) {
-        field = filter;
-    }
-    
     // Don't get to mess w/ other plugins objects
     return Object.assign(
         Object.create(null),
-        message ? message[field] : {}
+        message ? message[filter] : {}
     );
 };
