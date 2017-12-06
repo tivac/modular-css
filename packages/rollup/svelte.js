b59612f6a26631439f60e07a797f6e8ac6c2bfea
Pat Cavit <github@patcavit.com>
2017-12-06 15:21:40 -0800

    WIP: basic functionality
    BUT SO UGLY

--- packages/rollup/svelte.js
+++ packages/rollup/svelte.js
@@ -1,36 +1,73 @@
 "use strict";
 
+const fs = require("fs");
+
 const Processor = require("modular-css-core");
 
-module.exports = function(config, args) {
-    let processor = new Processor(args);
+module.exports = function(args) {
+    const files = Object.create(null);
+    const processor = new Processor(args);
 
-    return Object.assign({}, config, {
-        css : () => {
-            // Use this hook to write out the results to args.css
-        },
-
+    return {
         preprocess : {
-            markup : ({ content, options }) => {
-                console.log("markup");
-                console.log(content);
-                console.log(options.filename);
+            markup : ({ content, filename }) => {
+                const search = /<style[\S\s]*?>([\S\s]*?)<\/style>/igm;
+                
+                const[ ignored, style ] = search.exec(content);
 
-                return {
-                    code : content
-                };
+                return processor.string(
+                    filename,
+                    style
+                )
+                .then((result) => {
+                    const exported = result.files[result.file].exports;
+                    const regexp = new RegExp(`\{\{css.(${Object.keys(exported).join("|")})}}`, "gm");
+
+                    console.log(regexp.toString());
+                    
+
+                    return {
+                        code : content.replace(regexp, (match, key) => {
+                            if(!exported[key]) {
+                                throw new Error(`Mismatched key: ${match}`);
+                            }
+                            
+                            return exported[key].join(" ");
+                        })
+                    };
+                })
+                .catch((err) => {
+                    console.error(err.toString());
+                    
+                    throw err;
+                });
             },
 
-            style : ({ content, options }) => processor.string(
-                    options.filename,
-                    content
-                )
-                .then((result) => ({
-                    code : result.details.result.css
-                }))
-                .catch((err) => {
-                    throw err;
+            // Remove all the CSS, we'll write it out ourselves in the plugin
+            style : () => ({
+                code : "/* replaced by modular-css */"
+            })
+        },
+
+        plugin : {
+            name : "modular-css-rollup-svelte",
+
+            ongenerate : (bundle, result) => {
+                result.css = processor.output({
+                    to : args.css
+                });
+            },
+
+            onwrite : (bundle, result) => result.css.then((data) => {
+                    if(args.css) {
+                        // mkdirp.sync(path.dirname(options.css));
+                        
+                        fs.writeFileSync(
+                            args.css,
+                            data.css
+                        );
+                    }
                 })
         }
-    });
+    };
 };
--- packages/rollup/test/svelte.test.js
+++ packages/rollup/test/svelte.test.js
@@ -7,23 +7,26 @@ var fs = require("fs"),
     read  = require("test-utils/read.js")(__dirname),
     namer = require("test-utils/namer.js"),
     
-    plugin = require("../svelte.js");
+    svelte = require("../svelte.js");
 
 describe("/svelte.js", () => {
     afterEach(() => require("shelljs").rm("-rf", "./packages/rollup/test/output/*"));
     
-    it("should generate exports", () =>
-        rollup({
+    it("should generate exports", () => {
+        const parts = svelte({
+            css : "./packages/rollup/test/output/svelte.css"
+        });
+
+        return rollup({
             input   : require.resolve("./specimens/svelte.html"),
             plugins : [
-                require("rollup-plugin-svelte")(
-                    plugin({
-                        css : "./packages/rollup/test/output/svelte.css"
-                    })
-                )
+                require("rollup-plugin-svelte")({
+                    preprocess : parts.preprocess
+                }),
+                parts.plugin
             ]
         })
         .then((bundle) => bundle.generate({ format : "es" }))
-        .then((result) => expect(result.code).toMatchSnapshot())
-    );
+        .then((result) => expect(result.code).toMatchSnapshot());
+    });
 });
