61e720264d1d7ca62b4d10907a1ac1297d152b97
Pat Cavit <github@patcavit.com>
2018-06-07 23:45:32 -0700

    feat: rework svelte markup handling
    Also improve test coverage!

--- packages/svelte/src/markup.js
+++ packages/svelte/src/markup.js
@@ -6,79 +6,96 @@ const resolve = require("resolve-from");
 const dedent = require("dedent");
 const slash = require("slash");
 
-const styleRegex = /<style[\S\s]*?>([\S\s]*?)<\/style>/igm;
-const scriptRegex = /<script[\S\s]*?>([\S\s]*?)<\/script>/igm;
-const linkRegex = /<link\b[^<>]*?\bhref=\s*(?:"([^"]+)"|'([^']+)'|([^>\s]+))[^>]*>/igm;
+const styleRegex = /<style[\S\s]*?>([\S\s]*?)<\/style>/im;
+const scriptRegex = /<script[\S\s]*?>([\S\s]*?)<\/script>/im;
+const linkRegex = /<link\b[^<>]*?\bhref=\s*(?:"([^"]+)"|'([^']+)'|([^>\s]+))[^>]*>/im;
+
+function updateCss({ content, result }) {
+    const exported = result.files[result.file].exports;
+
+    return {
+        code : content
+            // Replace simple {css.<key>} values first
+            .replace(
+                new RegExp(`{css.(${Object.keys(exported).join("|")})}`, "gm"),
+                (match, key) => exported[key].join(" ")
+            )
+            // Then any remaining bare css.<key> values
+            .replace(
+                new RegExp(`(\\b)css.(${Object.keys(exported).join("|")})(\\b)`, "gm"),
+                (match, prefix, key, suffix) => `${prefix}"${exported[key].join(" ")}"${suffix}`
+            )
+    };
+}
+
+async function extractLink({ processor, content, filename, link }) {
+    // Remove the <link> element from the component to avoid double-loading
+    content = content.replace(link[0], "");
+
+    let external = slash(resolve(path.dirname(filename), link[1] || link[2] || link[3]));
+
+    const script = content.match(scriptRegex);
+
+    // To get rollup to watch the CSS file we need to inject an import statement
+    // if a <script> block already exists hijack it otherwise inject a simple one
+    if(script) {
+        content = content.replace(
+            script[0],
+            script[0].replace(script[1], dedent(`
+                import css from ${JSON.stringify(external)};
+                ${script[1]}
+            `))
+        );
+    } else {
+        content += dedent(`
+            <script>
+                import css from ${JSON.stringify(external)};
+            </script>
+        `);
+    }
+
+    const result = await processor.file(external);
+
+    return updateCss({ content, result });
+}
+
+async function extractStyle({ processor, content, filename, style }) {
+    const result = await processor.string(
+        filename,
+        style[1]
+    );
+
+    return updateCss({ content, result });
+}
 
 /* eslint-disable max-statements */
 module.exports = (processor) => ({ content, filename }) => {
-    const link = linkRegex.exec(content);
-    const style = styleRegex.exec(content);
-
-    if(!link && !style) {
-        return {
-            code : content
-        };
-    }
+    const link = content.match(linkRegex);
+    const style = content.match(styleRegex);
 
     if(link && style) {
         throw new Error("modular-css-svelte supports <style> OR <link>, but not both");
     }
 
-    let css;
-
     if(style) {
-        css = processor.string(
+        return extractStyle({
+            processor,
+            content,
             filename,
-            style[1]
-        );
+            style,
+        });
     }
 
     if(link) {
-        // Remove the <link> element from the component to avoid double-loading
-        content = content.replace(link[0], "");
-
-        let external = slash(resolve(path.dirname(filename), link[1] || link[2] || link[3]));
-
-        css = processor.file(external);
-
-        const script = scriptRegex.exec(content);
-        
-        // To get rollup to watch the CSS file we need to inject an import statement
-        // if a <script> block already exists hijack it
-        // otherwise inject a simple one
-        if(script) {
-            content = content.replace(
-                script[0],
-                script[0].replace(script[1], dedent(`
-                    import ${JSON.stringify(external)};
-                    ${script[1]}
-                `))
-            );
-        } else {
-            content += dedent(`
-                <script>
-                    import ${JSON.stringify(external)};
-                </script>
-            `);
-        }
+        return extractLink({
+            processor,
+            content,
+            filename,
+            link,
+        });
     }
-    
-    return css.then((result) => {
-        const exported = result.files[result.file].exports;
 
-        return {
-            code : content
-                // Replace simple {css.<key>} values first
-                .replace(
-                    new RegExp(`{css.(${Object.keys(exported).join("|")})}`, "gm"),
-                    (match, key) => exported[key].join(" ")
-                )
-                // Then any remaining bare css.<key> values
-                .replace(
-                    new RegExp(`(\\b)css.(${Object.keys(exported).join("|")})(\\b)`, "gm"),
-                    (match, prefix, key, suffix) => `${prefix}"${exported[key].join(" ")}"${suffix}`
-                )
-        };
-    });
+    return {
+        code : content
+    };
 };
