5facdff8927a0ceadb1083b3d689d1df396635b6
Pat Cavit <github@patcavit.com>
2018-07-10 23:35:57 -0700

    chore: tweak test config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 "use strict";
 
 module.exports = {
+    restoreMocks : true,
+    notify       : true,
+
     coveragePathIgnorePatterns : [
-      "node_modules",
-      "parsers",
-      "test-utils",
+      "/node_modules/",
+      "/parsers/",
+      "/test-utils/",
     ],
 
     watchPathIgnorePatterns : [
-        "output",
-        "specimens",
+        "/output/",
+        "/specimens/",
     ],
 };
