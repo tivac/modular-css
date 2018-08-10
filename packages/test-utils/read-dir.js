5d1b30477aa15cfd29af7abbae3a7add4f16b313
Pat Cavit <github@patcavit.com>
2018-08-09 22:45:57 -0700

    test: read whole dirs at once

---
+++ packages/test-utils/read-dir.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const fs = require("fs");
+const path = require("path");
+
+const read = require("read-dir-deep");
+
+module.exports = (cwd) =>
+    (name) => {
+        const dir = path.join(cwd, "./output", name);
+        const files = read.sync(dir);
+
+        return files.sort().map((file) => ({
+            file,
+            text : fs.readFileSync(path.join(dir, file), "utf8"),
+        }));
+    };
