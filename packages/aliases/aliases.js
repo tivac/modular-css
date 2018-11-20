031bd9315ab840624c77bbcf1344ff431e244d21
Pat Cavit <github@patcavit.com>
2018-11-19 22:59:20 -0800

    style: fix up some alignment

--- packages/aliases/aliases.js
+++ packages/aliases/aliases.js
@@ -4,16 +4,15 @@ const from = require("resolve-from");
 
 module.exports = (args) => {
     const options = Object.assign(
-            Object.create(null),
-            { aliases : {} },
-            args
-        );
+        Object.create(null),
+        { aliases : {} },
+        args
+    );
 
-    const aliases = Object.keys(options.aliases)
-        .map((alias) => ({
-            name   : alias,
-            search : new RegExp(`^${alias}\\b`),
-        }));
+    const aliases = Object.keys(options.aliases).map((alias) => ({
+        name   : alias,
+        search : new RegExp(`^${alias}\\b`),
+    }));
 
     return (src, file) => {
         const match = aliases.find(({ search }) => file.search(search) > -1);
