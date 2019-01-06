47c36141bf184e01710fbff8899aebc659ce344f
Pat Cavit <github@patcavit.com>
2019-01-05 23:02:45 -0800

    WIP: better docs for future-me

--- packages/www/rollup.config.js
+++ packages/www/rollup.config.js
@@ -22,15 +22,20 @@ module.exports = {
 
             browser : true,
         }),
+        
+        // Run webpack INSIDE ROLLUP to bundle postcss because it's fuckered otherwise
         require("./build/rollup-plugin-postcss.js")(),
+        
         require("rollup-plugin-commonjs")(),
         require("rollup-plugin-node-globals")(),
         require("rollup-plugin-node-builtins")(),
         require("rollup-plugin-json")(),
         require("rollup-plugin-svelte")(),
         
+        // Generate HTML skeleton including built files
         require("./build/rollup-plugin-html")(),
 
+        // Start a local server if in watch mode
         require("./build/rollup-plugin-sirv.js")(),
     ],
 };
