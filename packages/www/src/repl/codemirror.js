c70d7e619bee95b9ad77adee2cefa375343309b7
Pat Cavit <github@patcavit.com>
2019-01-16 23:22:03 -0800

    wip: remove all deps in bundle
    Since codemirror already provides it...

--- packages/www/src/repl/codemirror.js
+++ packages/www/src/repl/codemirror.js
@@ -3,13 +3,11 @@ import loadcss from "lazyload-css";
 import codemirror from "codemirror";
 import "codemirror/mode/css/css.js";
 
-import { dependencies } from "../../package.json";
-
 const theme = "base16-light";
 
 Promise.all([
-    loadcss(`https://unpkg.com/codemirror@${dependencies.codemirror}/lib/codemirror.css`),
-    loadcss(`https://unpkg.com/codemirror@${dependencies.codemirror}/theme/${theme}.css`),
+    loadcss(`https://unpkg.com/codemirror@${codemirror.version}/lib/codemirror.css`),
+    loadcss(`https://unpkg.com/codemirror@${codemirror.version}/theme/${theme}.css`),
 ]);
 
 export default codemirror;
