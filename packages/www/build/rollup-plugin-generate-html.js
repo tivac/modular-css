4fe679fdeb40b11cd8f6c7ca0e695a4bfae7719d
Pat Cavit <github@patcavit.com>
2019-01-17 00:05:59 -0800

    wip: syntax highlighting, heading fixups
    Also working on a nav sidebar for the guide but I need to make src/guide.html into its own template and break the header out into src/header.html that src/layout.html and src/guide.html can include.

--- packages/www/build/rollup-plugin-generate-html.js
+++ packages/www/build/rollup-plugin-generate-html.js
@@ -4,7 +4,13 @@ const fs = require("fs");
 const path = require("path");
 
 const shell = require("shelljs");
-const MD = require("markdown-it");
+const md = require("markdown-it")({
+    html        : true,
+    linkify     : true,
+    typographer : true,
+});
+
+const prism = require("markdown-it-prism");
 const { default : toc } = require("markdown-it-toc-and-anchor");
 
 const svelte = require("./svelte.js");
@@ -12,22 +18,21 @@ const { version } = require("../package.json");
 
 const type = "md";
 
+const prismTheme = "https://unpkg.com/prismjs@1.15.0/themes/prism-tomorrow.css";
+
 module.exports = ({ preprocess }) => {
     const outputs = new Map();
-
     let guides;
+    const tocs = [];
 
-    // Set up markdown renderer
-    const md = new MD({
-        html        : true,
-        linkify     : true,
-        typographer : true,
-    });
-    
+    // Set up markdown plugins
     md.use(toc, {
-        tocFirstLevel           : 3,
-        wrapHeadingTextInAnchor : true,
+        tocFirstLevel   : 3,
+        tocClassName    : "toc",
+        anchorClassName : "anchor",
     });
+
+    md.use(prism);
 
     return {
         name : "rollup-plugin-generate-html",
@@ -71,10 +76,10 @@ module.exports = ({ preprocess }) => {
                     
                     // TODO: Remove this somehow, the lazy-loading in
                     // codemirror.js breaks codemirror for some reason
-                    styles : styles.concat(
+                    styles : styles.concat([
                         "https://unpkg.com/codemirror@5.24.2/lib/codemirror.css",
                         "https://unpkg.com/codemirror@5.24.2/theme/monokai.css",
-                    ),
+                    ]),
                     
                     scripts : `
                     <script>
@@ -94,25 +99,36 @@ module.exports = ({ preprocess }) => {
                 })
             );
 
+            const html = guides
+                .map((file) => {
+                    let text = fs.readFileSync(file, "utf8");
+                
+                    // Prefix headers to decrement them all down a level
+                    text = text.replace(/^#/gm, "##");
+                    
+                    return md.render(text, {
+                        tocCallback : (tocmd, headings, tochtml) =>
+                            tocs.push(
+                                `<li class="doc"><a href="#${headings[0].anchor}">${headings[0].content}</a></li>`,
+                                tochtml
+                            )
+                    });
+                })
+                .join("\n");
+
             // Write out guide page
             outputs.set(
                 path.join(dir, "./guide/index.html"),
                 page.render({
-                    styles,
-                    title   : "Guide",
+                    title  : "Guide",
+                    styles : styles.concat([
+                        prismTheme,
+                    ]),
                     content : layout.render({
                         version,
-                        type,
-                        content : guides
-                            .map((file) => {
-                                let text = fs.readFileSync(file, "utf8");
-                            
-                                // Prefix headers to decrement them all down a level
-                                text = text.replace(/^#/gm, "##");
-                                
-                                return md.render(text);
-                            })
-                            .join("\n"),
+                        type    : "guide",
+                        content : html,
+                        sidebar : tocs.join("\n"),
                     }),
                 })
             );
@@ -121,10 +137,12 @@ module.exports = ({ preprocess }) => {
             outputs.set(
                 path.join(dir, "./index.html"),
                 page.render({
-                    styles,
-                    type,
+                    styles : styles.concat([
+                        prismTheme,
+                    ]),
                     content : layout.render({
                         version,
+                        type    : "home",
                         content : md.render(
                             fs.readFileSync(require.resolve("../src/index.md"), "utf8")
                         ),
