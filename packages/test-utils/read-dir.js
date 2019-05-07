80210cef0d18219c3d3d700512d64e75303416e3
Pat Cavit <github@patcavit.com>
2019-05-06 23:32:02 -0700

    chore: more :package: :lock: and :recycle: changes

--- packages/test-utils/read-dir.js
+++ packages/test-utils/read-dir.js
@@ -8,7 +8,7 @@ const { readDirDeepSync : read } = require("read-dir-deep");
 module.exports = (cwd) =>
     (name) => {
         const dir = path.join(cwd, "./output", name);
-        const files = read.sync(dir);
+        const files = read(dir);
 
         return files.sort().reduce((out, file) => {
             out[file.replace(/\\/g, "/")] = fs.readFileSync(path.join(dir, file), "utf8");
