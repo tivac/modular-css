bacd15457ba14920433c6665c8285f8b36913f1e
Pat Cavit <github@patcavit.com>
2019-07-24 23:57:35 -0700

    FINALLY FIXED IT

--- packages/www/rollup.config.js
+++ packages/www/rollup.config.js
@@ -40,69 +40,6 @@ const shared = (store) => {
 const bundle = shared();
 
 module.exports = [
-    // Browser build of the REPL runs first, we need to know what the filename will be
-    {
-        input : {
-            repl : "./src/repl/index.js",
-        },
-
-        output : {
-            dir    : dest,
-            format : "esm",
-
-            sourcemap : true,
-
-            entryFileNames : "[name]-[hash].js",
-        },
-
-        plugins : [
-            // Wipe the destination dir on each rebuild
-            require("./build/rollup-plugin-clean")(),
-
-            require("rollup-plugin-alias")({
-                fs   : require.resolve("./stubs/fs.js"),
-                path : require.resolve("./stubs/path.js"),
-            }),
-
-            require("rollup-plugin-node-resolve")({
-                mainFields : [ "module", "browser", "main" ],
-
-                preferBuiltins : false,
-            }),
-            
-            // Run webpack INSIDE ROLLUP to bundle postcss because it's fuckered otherwise
-            require("./build/rollup-plugin-postcss.js")(),
-            
-            require("rollup-plugin-commonjs")(),
-            require("rollup-plugin-node-globals")(),
-            require("rollup-plugin-node-builtins")(),
-            require("rollup-plugin-json")(),
-            
-            require("rollup-plugin-svelte")({
-                preprocess,
-            }),
-
-            require("@modular-css/rollup")({
-                processor,
-                meta : "meta1.json",
-            }),
-
-            // Weird little inline plugin to make the previous bundle available
-            // to the next build. Needed so it can figure out what this build named files.
-            {
-                name : "rollup-plugin-sharing",
-                generateBundle(options, chunks) {
-                    bundle(chunks);
-                },
-            },
-            
-            // Start a local server if in watch mode
-            isWatch && require("./build/rollup-plugin-sirv.js")(),
-
-            // Compress JS in production mode
-            isProduction && require("rollup-plugin-terser").terser(),
-        ],
-    },
     // CJS Build of home/guide for node generation
     {
         input : {
@@ -135,6 +72,10 @@ module.exports = [
         },
 
         plugins : [
+            // Wipe the destination dir on each rebuild
+            require("./build/rollup-plugin-clean")(),
+
+
             require("./build/rollup-plugin-add-watch-files.js")(),
 
             require("rollup-plugin-node-resolve")({
@@ -153,12 +94,73 @@ module.exports = [
 
             require("@modular-css/rollup")({
                 processor,
-                meta : "meta2.json",
+            }),
+
+            // Weird little inline plugin to make the previous bundle available
+            // to the next build. Needed so it can figure out what this build named files.
+            {
+                name : "rollup-plugin-sharing",
+                generateBundle(options, chunks) {
+                    bundle(chunks);
+                },
+            },
+
+            // Start a local server if in watch mode
+            isWatch && require("./build/rollup-plugin-sirv.js")(),
+
+            // Compress JS in production mode
+            isProduction && require("rollup-plugin-terser").terser(),
+        ],
+    },
+    // Browser build of the REPL
+    {
+        input : {
+            repl : "./src/repl/index.js",
+            
+            // Including to force the CSS to be split apart so the REPL CSS
+            // doesn't duplicate things it doesn't need to
+            page : "./src/page.html",
+        },
+
+        output : {
+            dir    : dest,
+            format : "esm",
+
+            sourcemap : true,
+
+            entryFileNames : "[name]-[hash].js",
+        },
+
+        plugins : [
+            require("rollup-plugin-alias")({
+                fs   : require.resolve("./stubs/fs.js"),
+                path : require.resolve("./stubs/path.js"),
+            }),
+
+            require("rollup-plugin-node-resolve")({
+                mainFields : [ "module", "browser", "main" ],
+
+                preferBuiltins : false,
+            }),
+            
+            // Run webpack INSIDE ROLLUP to bundle postcss because it's fuckered otherwise
+            require("./build/rollup-plugin-postcss.js")(),
+            
+            require("rollup-plugin-commonjs")(),
+            require("rollup-plugin-node-globals")(),
+            require("rollup-plugin-node-builtins")(),
+            require("rollup-plugin-json")(),
+            
+            require("rollup-plugin-svelte")({
+                preprocess,
+            }),
+
+            require("@modular-css/rollup")({
+                processor,
             }),
 
             // Generate HTML for all the static pages
             require("./build/rollup-plugin-generate-html.js")({ bundle }),
         ],
     },
-    
 ];
