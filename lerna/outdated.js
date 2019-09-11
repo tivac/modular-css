23492f508a8f065d9dd16c21beb147f31961e8fc
Pat Cavit <github@patcavit.com>
2019-09-10 23:33:50 -0700

    chore: add custom outdated script
    `npm outdated` run in every package is full of noise from bootstrapped packages, this is much more minimal and useful.

---
+++ lerna/outdated.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const shell = require("shelljs");
+
+const name = process.env.LERNA_PACKAGE_NAME;
+
+const info = shell.exec("npm outdated --json", { silent : true }).stdout;
+
+const packages = JSON.parse(info);
+const keys = Object.keys(packages);
+
+if(!keys.length) {
+    return;
+}
+
+keys.forEach((key) => {
+    const { wanted, latest } = packages[key];
+
+    if(wanted === "linked" && latest === "linked") {
+        return;
+    }
+
+    if(wanted === latest) {
+        return;
+    }
+
+    // TODO: Better formatting someday
+    console.log(name, key, { wanted, latest });
+});
