4f36c96678c53a2007c01b79ad4ab6d4d4e97158
Pat Cavit <github@patcavit.com>
2020-01-04 22:32:52 -0800

    WIP: improve coverage

--- packages/processor/test/api-unused.test.js
+++ packages/processor/test/api-unused.test.js
@@ -16,18 +16,26 @@ describe("/processor.js", () => {
         });
 
         it("should return a map of unused exports", async () => {
-            await processor.string(
-                "./one.css",
-                ".one1 { } .one2 { }",
+            await processor.file("./packages/processor/test/specimens/simple.css");
+            await processor.file("./packages/processor/test/specimens/start.css");
+            await processor.file("./packages/processor/test/specimens/at-composes.css");
+            await processor.file("./packages/processor/test/specimens/composes-global.css");
+            await processor.file("./packages/processor/test/specimens/global.css");
+
+            processor._used(
+                processor._normalize("./packages/processor/test/specimens/simple.css"),
+                "wooga"
             );
             
-            await processor.string(
-                "./two.css",
-                ".two1 { } .two2 { }",
+            processor._used(
+                processor._normalize("./packages/processor/test/specimens/start.css"),
+                "wooga"
             );
 
-            processor._used(processor._normalize("./one.css"), "one1");
-            processor._used(processor._normalize("./two.css"), "two2");
+            processor._used(
+                processor._normalize("./packages/processor/test/specimens/composes-global.css"),
+                "two",
+            );
 
             const unused = processor.unused();
 
