c6eaf96b2c581aa5708c0a567f7829887c2f6121
Pat Cavit <github@patcavit.com>
2020-10-23 15:06:12 -0700

    WIP: adjust test infra

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,29 @@
 "use strict";
 
 module.exports = {
-    projects : [
-        {
-            displayName  : "tests",
-            restoreMocks : true,
-            notify       : true,
+    restoreMocks : true,
+    notify       : true,
 
-            coveragePathIgnorePatterns : [
-                "/node_modules/",
-                "/parsers/",
-                "/packages/test-utils/",
-            ],
+    coveragePathIgnorePatterns : [
+        "/node_modules/",
+        "/parsers/",
+        "/packages/test-utils/",
+    ],
 
-            watchPathIgnorePatterns : [
-                "/output/",
-                "/specimens/",
-            ],
+    watchPathIgnorePatterns : [
+        "/output/",
+        "/specimens/",
+    ],
 
-            setupFilesAfterEnv : [
-                "<rootDir>/packages/test-utils/expect/toMatchDiffSnapshot.js",
-                "<rootDir>/packages/test-utils/expect/toMatchLogspySnapshot.js",
-                "<rootDir>/packages/test-utils/expect/toMatchRollupAssetSnapshot.js",
-                "<rootDir>/packages/test-utils/expect/toMatchRollupCodeSnapshot.js",
-                "<rootDir>/packages/test-utils/expect/toMatchRollupSnapshot.js",
-            ],
+    setupFilesAfterEnv : [
+        "<rootDir>/packages/test-utils/expect/toMatchDiffSnapshot.js",
+        "<rootDir>/packages/test-utils/expect/toMatchLogspySnapshot.js",
+        "<rootDir>/packages/test-utils/expect/toMatchRollupAssetSnapshot.js",
+        "<rootDir>/packages/test-utils/expect/toMatchRollupCodeSnapshot.js",
+        "<rootDir>/packages/test-utils/expect/toMatchRollupSnapshot.js",
+    ],
 
-            snapshotSerializers : [
-                require.resolve("snapshot-diff/serializer.js"),
-            ],
-        },
+    snapshotSerializers : [
+        require.resolve("snapshot-diff/serializer.js"),
     ],
 };
