7120558142c51a58a2e8423c25c51ae27ae5c9f7
Pat Cavit <github@patcavit.com>
2021-06-25 00:14:20 -0700

    refactor: use prepare() hook for cleanup
    Need to roll this out further

--- packages/processor/plugins/keyframes.js
+++ packages/processor/plugins/keyframes.js
@@ -4,46 +4,49 @@ const escape = require("escape-string-regexp");
 
 const message = require("../lib/message.js");
 
-module.exports = () => {
-    let refs;
-    let search;
+module.exports = () => ({
+    postcssPlugin : "modular-css-keyframes",
 
-    return {
-        postcssPlugin : "modular-css-keyframes",
+    prepare(result) {
+        const refs = message(result, "keyframes");
 
-        Once(root, { result }) {
-            refs = message(result, "keyframes");
+        const search = new RegExp(
+            Object.keys(refs)
+            .map((ref) => `(\\b${escape(ref)}\\b)`)
+            .join("|"),
+            "g"
+        );
+        
+        console.log({ refs, search, msgs : result.messages });
+        
+        return {
+            Declaration : {
+                // Go look up "animation" declarations and rewrite their names to scoped values
+                animation(decl) {
+                    console.log("animation", decl.value);
 
-            search = new RegExp(
-                Object.keys(refs)
-                    .map((ref) => `(\\b${escape(ref)}\\b)`)
-                    .join("|"),
-                "g"
-            );
-        },
+                    // Early out if there's nothing to replace
+                    if(!Object.keys(refs).length) {
+                        return;
+                    }
 
-        Declaration : {
-            // Go look up "animation" declarations and rewrite their names to scoped values
-            animation(decl, { result }) {
-                // Early out if there's nothing to replace
-                if(!Object.keys(refs).length) {
-                    return;
-                }
+                    decl.value = decl.value.replace(search, (match) => refs[match]);
+                },
+    
+                // Go look up "animation-name" declarations and rewrite their names to scoped values
+                ["animation-name"](decl) {
+                    // Early out if there's nothing to replace
+                    if(!Object.keys(refs).length) {
+                        return;
+                    }
 
-                decl.value = decl.value.replace(search, (match) => refs[match]);
+                    console.log("animation-name", decl.value);
+    
+                    decl.value = decl.value.replace(search, (match) => refs[match]);
+                },
             },
-
-            // Go look up "animation-name" declarations and rewrite their names to scoped values
-            ["animation-name"](decl, { result }) {
-                // Early out if there's nothing to replace
-                if(!Object.keys(refs).length) {
-                    return;
-                }
-
-                decl.value = decl.value.replace(search, (match) => refs[match]);
-            },
-        },
-    };
-};
+        };
+    },
+});
 
 module.exports.postcss = true;
