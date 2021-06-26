76cebc07ac3b93b7f393d6af8ab8d080649454b1
Pat Cavit <github@patcavit.com>
2021-06-26 00:35:00 -0700

    refactor: using prepare()

--- packages/processor/plugins/at-composes.js
+++ packages/processor/plugins/at-composes.js
@@ -6,62 +6,60 @@ const { selectorKey } = require("../lib/keys.js");
 
 const plugin = "modular-css-at-composes";
 
-module.exports = () => {
-    let source;
+module.exports = () => ({
+    postcssPlugin : plugin,
 
-    return {
-        postcssPlugin : plugin,
+    prepare(result) {
+        let source = false;
 
-        Once() {
-            source = false;
-        },
-        
-        AtRule : {
-            composes(rule, { result }) {
-                if(source) {
-                    throw rule.error(`Only one @composes rule per file`, { word : "composes" });
-                }
-
-                const { from, processor } = result.opts;
-                const { files, graph } = processor;
-        
-                // We know it's safe, otherwise it would have failed in graph-nodes pass
-                const parsed = parser.parse(rule.params);
-        
-                source = files[processor.resolve(from, parsed.source)];
-        
-                // Remove the @composes from the output
-                rule.remove();
-
-                // Create a copy of each defined class and also the dependency graph (if it has dependencies)
-                const atcomposes = Object.keys(source.classes).reduce((acc, key) => {
-                    acc[key] = [ ...source.classes[key] ];
-
-                    const skey = selectorKey(source.name, key);
-
-                    if(!graph.hasNode(skey)) {
-                        return acc;
+        return {
+            AtRule : {
+                composes(rule) {
+                    if(source) {
+                        throw rule.error(`Only one @composes rule per file`, { word : "composes" });
                     }
 
-                    const dkey = processor._addSelector(from, key);
-
-                    // Duplicate the source key's info to the new dependent key
-                    graph.dependenciesOf(skey).forEach((dep) => {
-                        graph.addDependency(dkey, dep);
-                    });
-
-                    return acc;
-                }, Object.create(null));
-
-                result.messages.push({
-                    type : "modular-css",
-                    plugin,
+                    const { from, processor } = result.opts;
+                    const { files, graph } = processor;
             
-                    atcomposes,
-                });
+                    // We know it's safe, otherwise it would have failed in graph-nodes pass
+                    const parsed = parser.parse(rule.params);
+            
+                    source = files[processor.resolve(from, parsed.source)];
+            
+                    // Remove the @composes from the output
+                    rule.remove();
+
+                    // Create a copy of each defined class and also the dependency graph (if it has dependencies)
+                    const atcomposes = Object.keys(source.classes).reduce((acc, key) => {
+                        acc[key] = [ ...source.classes[key] ];
+
+                        const skey = selectorKey(source.name, key);
+
+                        if(!graph.hasNode(skey)) {
+                            return acc;
+                        }
+
+                        const dkey = processor._addSelector(from, key);
+
+                        // Duplicate the source key's info to the new dependent key
+                        graph.dependenciesOf(skey).forEach((dep) => {
+                            graph.addDependency(dkey, dep);
+                        });
+
+                        return acc;
+                    }, Object.create(null));
+
+                    result.messages.push({
+                        type : "modular-css",
+                        plugin,
+                
+                        atcomposes,
+                    });
+                },
             },
-        },
-    };
-};
+        };
+    },
+});
 
 module.exports.postcss = true;
