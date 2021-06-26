988be54735b14b69c285d0c901205f364c8bfbab
Pat Cavit <github@patcavit.com>
2021-06-26 00:36:47 -0700

    refactor: more prepare()

--- packages/processor/plugins/externals.js
+++ packages/processor/plugins/externals.js
@@ -6,60 +6,58 @@ const parser = require("../parsers/external.js");
 
 // Find :external(<rule> from <file>) references and update them to be
 // the namespaced selector instead
-module.exports = () => {
-    let process;
+module.exports = () => ({
+    postcssPlugin : "modular-css-externals",
 
-    return {
-        postcssPlugin : "modular-css-externals",
+    prepare(result) {
+        const { processor, from } = result.opts;
 
-        Once(css, { result }) {
-            const { processor, from } = result.opts;
+        const process = (rule, pseudo) => {
+            const params = pseudo.nodes.toString();
+            const { source, ref } = parser.parse(params);
+            const { name } = ref;
+            const file = processor.files[processor.resolve(from, source)];
 
-            process = (rule, pseudo) => {
-                const params = pseudo.nodes.toString();
-                const { source, ref } = parser.parse(params);
-                const { name } = ref;
-                const file = processor.files[processor.resolve(from, source)];
+            if(!file.classes[name]) {
+                throw rule.error(`Invalid external reference: ${name}`, { word : name });
+            }
 
-                if(!file.classes[name]) {
-                    throw rule.error(`Invalid external reference: ${name}`, { word : name });
-                }
+            // This was a... poor naming choice
+            const s = selector.selector();
 
-                // This was a... poor naming choice
-                const s = selector.selector();
+            file.classes[name].forEach((value) =>
+                s.append(selector.className({ value }))
+            );
 
-                file.classes[name].forEach((value) =>
-                    s.append(selector.className({ value }))
-                );
+            const root = selector.root();
 
-                const root = selector.root();
+            root.append(s);
 
-                root.append(s);
+            pseudo.replaceWith(root);
+        };
 
-                pseudo.replaceWith(root);
-            };
-        },
-
-        Rule(rule) {
-            const externals = selector((selectors) => {
-                const found = [];
-
-                selectors.walkPseudos((pseudo) => {
-                    // Need to ensure we only process :external pseudos, see #261
-                    if(pseudo.value !== ":external") {
-                        return;
-                    }
-
-                    // Can't replace here, see postcss/postcss-selector-parser#105
-                    found.push(pseudo);
+        return {
+            Rule(rule) {
+                const externals = selector((selectors) => {
+                    const found = [];
+    
+                    selectors.walkPseudos((pseudo) => {
+                        // Need to ensure we only process :external pseudos, see #261
+                        if(pseudo.value !== ":external") {
+                            return;
+                        }
+    
+                        // Can't replace here, see postcss/postcss-selector-parser#105
+                        found.push(pseudo);
+                    });
+    
+                    found.forEach((pseudo) => process(rule, pseudo));
                 });
-
-                found.forEach((pseudo) => process(rule, pseudo));
-            });
-
-            rule.selector = externals.processSync(rule);
-        },
-    };
-};
+    
+                rule.selector = externals.processSync(rule);
+            },
+        };
+    },
+});
 
 module.exports.postcss = true;
