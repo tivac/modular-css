023e6342eeb1796180bdff0a837f9487abf94991
Pat Cavit <github@patcavit.com>
2021-06-29 00:02:12 -0700

    test: combine keyframes & scoping

--- packages/processor/test/scoping.test.js
+++ packages/processor/test/scoping.test.js
@@ -18,11 +18,7 @@ describe("/processor.js", () => {
             const { exports } = await processor.string(
                 "./simple.css",
                 dedent(`
-                    @keyframes kooga {
-                        from {}
-                        to {}
-                        50% {}
-                    }
+                    @keyframes kooga {}
                     #fooga { }
                     .wooga { }
                     .one,
@@ -30,10 +26,6 @@ describe("/processor.js", () => {
                     .one { }
                     @media print {
                         .three {}
-                    }
-                    .anim {
-                        animation: kooga 50s ease-in;
-                        animation-name: something-else;
                     }
                 `)
             );
@@ -128,6 +120,79 @@ describe("/processor.js", () => {
                 "./invalid/global.css",
                 ".a :global() { }"
             )).rejects.toThrow(`:global(...) must not be empty`);
+        });
+
+        it("should leave unknown animation names alone", async () => {
+            await processor.string("./unknown-name.css", dedent(`
+                .a { animation: a; }
+                .b { animation-name: b; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
+        });
+
+        it("should not scope rules within @keyframes", async () => {
+            await processor.string("./unknown-name.css", dedent(`
+                @keyframes a {
+                    from { color: white; }
+                    50.25% { color: red; }
+                    to { color: black; }
+                }
+
+                .a { animation: a; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
+        });
+        
+        it("should update scoped animations from the scoping plugin's message", async () => {
+            await processor.string("./animation.css", dedent(`
+                @keyframes a {}
+                .b { animation: a; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
+        });
+
+        it("should update the animation-name property", async () => {
+            await processor.string("./animation-name.css", dedent(`
+                @keyframes a {}
+                .b { animation-name: a; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
+        });
+
+        // Issue 208
+        it("should update multiple animations properly", async () => {
+            await processor.string("./multiple-animations.css", dedent(`
+                @keyframes a {}
+                @keyframes b {}
+                .c { animation: a 10s linear, b 0.2s infinite; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
+        });
+
+        it("should update scoped prefixed animations from the scoping plugin's message", async () => {
+            await processor.string("./prefixed-animations.css", dedent(`
+                @-webkit-keyframes a {}
+                .b { animation: a; }
+            `));
+
+            const { css } = await processor.output();
+
+            expect(css).toMatchSnapshot();
         });
     });
 });
