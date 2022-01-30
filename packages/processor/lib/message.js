f858a101e2c8061addbb5a197d0e5a0909d965ee
Pat Cavit <github@patcavit.com>
2022-01-30 00:18:15 -0800

    chore(processor): remove lodash

--- packages/processor/lib/message.js
+++ packages/processor/lib/message.js
@@ -1,9 +1,13 @@
 "use strict";
 
-const find = require("lodash/findLast");
-
 module.exports = ({ messages }, filter) => {
-    const message = find(messages, filter);
+    let message;
+    
+    for(let i = 0; i < messages.length; i++) {
+        if(filter(messages[i])) {
+            message = messages[i]
+        }
+    }
 
     // Don't get to mess w/ other plugins objects
     return {
