tivac/modular-css

commit 9391000209001e0f3b74f0404f59c584b76dfe8a

chore: latest sveltekit for m-css.com

packages/www/svelte.config.js
@@ -1,9 +1,7 @@
 import path from "path";
 
 import staticAdapter from "@sveltejs/adapter-static";
-import vitePluginInspector from "vite-plugin-inspect";
 import { NodeModulesPolyfillPlugin as modulesPolyfill } from "@esbuild-plugins/node-modules-polyfill";
-import { NodeGlobalsPolyfillPlugin as globalsPolyfill } from "@esbuild-plugins/node-globals-polyfill";
 
 import preprocessor from "@modular-css/svelte";
 import mcss from "@modular-css/vite";
@@ -55,16 +53,8 @@ const config = {
             pages : "dist",
         }),
 
-        // hydrate the <body> element in src/app.html, the default
-        // is a <div> but I don't understand why that is necessary
-        // and think the resulting HTML looks kinda sloppy
-        target : "body",
-
         vite : {
             plugins : [
-                // Vite plugin inspector
-                vitePluginInspector(),
-
                 // Bundle @modular-css/processor and its dependencies via
                 // embedded rollup within rollup because vite doesn't handle
                 // cjs modules and node globals very well
@@ -91,9 +81,6 @@ const config = {
                 esbuildOptions : {
                     plugins : [
                         modulesPolyfill(),
-                        globalsPolyfill({
-                            process : true,
-                        }),
                     ],
                 },
             },

packages/www/src/app.html
@@ -6,9 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<link href="https://unpkg.com/normalize.css@8.0.1/normalize.css" rel="stylesheet" />
 	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet" />
-	%svelte.head%
+	%sveltekit.head%
 </head>
 <body>
-	%svelte.body%
+	%sveltekit.body%
 </body>
 </html>

commit 58af5ecd2ef9439440c8b41b16fdb597efd7c478

chore: fix site not creating HTML
This broke with 9391000209001e0f3b74f0404f59c584b76dfe8a when I updated `@sveltejs/adapter-static` because apparently a default changed somewhere?

packages/www/svelte.config.js
@@ -53,6 +53,12 @@ const config = {
             pages : "dist",
         }),
 
+        // Force pre-rendering for all pages
+        // Why doesn't adapter-static do this automatically any more?
+        prerender : {
+            default : true,
+        },
+
         vite : {
             plugins : [
                 // Bundle @modular-css/processor and its dependencies via