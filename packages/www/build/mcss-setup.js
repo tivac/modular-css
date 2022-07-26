68a30de8c1943406a78e586a1a5743a10c5f00a0
Pat Cavit <github@patcavit.com>
2022-07-25 19:46:23 -0700

    fix: m-css.com on vite@3

---
+++ packages/www/build/mcss-setup.js
@@ -0,0 +1,39 @@
+import preprocessor from "@modular-css/svelte";
+import aliases from "@modular-css/path-aliases";
+import namer from "@modular-css/shortnames";
+import postcssNested from "postcss-nested";
+
+const isProd = process.env.NODE_ENV === "production";
+
+// Set up the svelte preprocessor and get a reference to the
+// mcss processor so we can pass it into the vite plugin
+const { preprocess, processor } = preprocessor({
+    // Default is .css but we need .mcss because of vite
+    include : /\.mcss$/i,
+    
+    // These cause no end of warnings from the processor because
+    // they don't make nice JS identifiers usually, so disabled
+    exportGlobals : false,
+
+    // Enable for debugging, but disabled because NOISY
+    verbose : false,
+
+    // Crank down names in prod to be itty-bitty
+    namer : isProd ? namer() : null,
+
+    // Bring sveltekit aliases into m-css
+    resolvers : [
+        aliases({
+            aliases : {
+                $lib : "./src/lib",
+            },
+        }),
+    ],
+    
+    // I like nesting, so sue me
+    before : [
+        postcssNested(),
+    ],
+});
+
+export { preprocess, processor };
