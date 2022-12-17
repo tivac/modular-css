989d1f06caf0505d7cc7bfee52f29afd5a0a7760
Pat Cavit <github@patcavit.com>
2022-12-16 16:23:35 -0800

    feat(vite): vite@4 support & test infra (#919)

---
+++ playwright.config.js
@@ -0,0 +1,38 @@
+/** @type {import("@playwright/test").PlaywrightTestConfig} */
+const config = {
+	testDir : "./packages/vite/tests",
+    // baseURL : "http://127.0.0.1:5173",
+    
+	timeout : 30 * 1000,
+	
+	expect : {
+		timeout : 5 * 1000,
+	},
+	
+	// Run tests in files in parallel
+	fullyParallel : true,
+
+	// Fail the build on CI if you accidentally left test.only in the source code.
+	forbidOnly : Boolean(process.env.CI),
+
+	// Retry on CI only
+	retries : process.env.CI ? 2 : 0,
+	
+    // Opt out of parallel tests on CI.
+	workers : process.env.CI ? 1 : undefined,
+	
+    use : {
+		screenshot : "only-on-failure",
+        trace      : "on-first-retry",
+        video      : "on-first-retry",
+	},
+
+    webServer : {
+		command             : "npm run start:vite",
+        port                : 5173,
+		timeout             : 30 * 1000,
+		reuseExistingServer : true,
+	},
+};
+
+module.exports = config;
