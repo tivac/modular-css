2b9ed881d06359de122caf880a816699318921de
Pat Cavit <github@patcavit.com>
2025-12-19 15:19:38 -0800

    chore: remove lint-staged
    Blowing up on windows for _reasons_

--- .lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-module.exports = {
-    // Rebuild parsing code any time the sources change
-    "*.pegjs" : "node parsers/build.js",
-};
--- .simple-git-hooks.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-module.exports = {
-    "pre-commit" : "npx lint-staged",
-};
